f539660b649f5bcd5ed56351625a295ec6850677
Mötz Jensen <motz@e-s.dk>
2018-09-29 23:34:54 +0200

    Extend Add-AadUserSecurity with documentation

--- d365fo.tools/internal/functions/add-aadusersecurity.ps1
+++ d365fo.tools/internal/functions/add-aadusersecurity.ps1
@@ -1,5 +1,44 @@
-﻿function Add-AadUserSecurity ($sqlCommand,$Id) {
-    
+﻿<#
+.SYNOPSIS
+Assign D365 Security configuration
+
+.DESCRIPTION
+Assign the same security configuration as the ADMIN user in the D365FO database
+
+.PARAMETER sqlCommand
+The SQL Command object that should be used when assigning the permissions
+
+.PARAMETER Id
+Id of the user inside the D365FO database
+
+.EXAMPLE
+$SqlParams = @{
+                DatabaseServer = "localhost"
+                DatabaseName = "AXDB"
+                SqlUser = "sqladmin"
+                SqlPwd = "Pass@word1"
+                TrustedConnection = $false
+            }
+        
+$SqlCommand = Get-SqlCommand @SqlParams
+Add-AadUserSecurity -SqlCommand $SqlCommand -Id "TestUser"
+
+This will create a new Sql Command object using the Get-SqlCommand cmdlet and the $SqlParams hashtable containing all the needed parameters.
+With the $SqlCommand in place it calls the Add-AadUserSecurity cmdlet and instructs it to update the "TestUser" to have the same security configuration as the ADMIN user.
+
+.NOTES
+Author: Rasmus Andersen (@ITRasmus)
+
+#>
+function Add-AadUserSecurity {
+    [OutputType([System.Boolean])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.Data.SqlClient.SqlCommand] $SqlCommand,
+
+        [Parameter(Mandatory = $true)]
+        [string] $Id
+    )
 
     $commandText = (Get-Content "$script:ModuleRoot\internal\sql\Set-AadUserSecurityInD365FO.sql") -join [Environment]::NewLine
    
@@ -15,5 +54,5 @@
 
     $SqlCommand.Parameters.Clear()
 
-    return $differenceBetweenNewUserAndAdmin -eq 0
+    $differenceBetweenNewUserAndAdmin -eq 0
 }
