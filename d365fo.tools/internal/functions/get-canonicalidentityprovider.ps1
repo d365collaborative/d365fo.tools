cf0b57be2bfa3e9648bdd13091a263e0839917e5
Mötz Jensen <motz@e-s.dk>
2018-10-02 07:06:47 +0200

    Fix more unit test fails

--- d365fo.tools/internal/functions/get-canonicalidentityprovider.ps1
+++ d365fo.tools/internal/functions/get-canonicalidentityprovider.ps1
@@ -1,22 +1,40 @@
-﻿
-function Get-CanonicalIdentityProvider {
+﻿<#
+.SYNOPSIS
+Load the Canonical Identity Provider
 
+.DESCRIPTION
+Load the necessary dll files from the D365 instance to get the Canonical Identity Provider object
+
+.EXAMPLE
+Get-CanonicalIdentityProvider
+
+This will get the Canonical Identity Provider from the D365 instance
+
+.NOTES
+Author: Rasmus Andersen (@ITRasmus)
+Author: Mötz Jensen (@Splaxi)
+
+#>
+function Get-CanonicalIdentityProvider {
+    [CmdletBinding()]
+    param ()
     try {
+        Write-PSFMessage -Level Verbose "Loading dll files to do some work against the CanonicalIdentityProvider."
+
         Add-Type -Path "$Script:AOSPath\bin\Microsoft.Dynamics.AX.Framework.EncryptionEngine.dll"
         Add-Type -Path "$Script:AOSPath\bin\Microsoft.Dynamics.AX.Security.AuthenticationCommon.dll"
 
+        Write-PSFMessage -Level Verbose "Executing the CanonicalIdentityProvider lookup logic."
         $Identity = [Microsoft.Dynamics.AX.Security.AuthenticationCommon.AadHelper]::GetIdentityProvider()
         $Provider = [Microsoft.Dynamics.AX.Security.AuthenticationCommon.AadHelper]::GetCanonicalIdentityProvider($Identity)
-        Write-Verbose "CanonicalIdentityProvider $Provider"
 
-        
+        Write-PSFMessage -Level Verbose "CanonicalIdentityProvider is: $Provider" -Tag $Provider
 
         return $Provider
-
     }
-    catch [System.Reflection.ReflectionTypeLoadException] {
-        Write-Output "Message: $($_.Exception.Message)"
-        Write-Output "StackTrace: $($_.Exception.StackTrace)"
-        Write-Output "LoaderExceptions: $($_.Exception.LoaderExceptions)"
+    catch {
+        Write-PSFMessage -Level Host -Message "Something went wrong while working against the CanonicalIdentityProvider." -Exception $PSItem.Exception
+        Stop-PSFFunction -Message "Stopping because of errors"
+        return
     }
 }
