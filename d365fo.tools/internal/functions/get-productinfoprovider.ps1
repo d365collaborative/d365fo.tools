3c2ff103cf7d548d7356df19120da107f6122c0b
Mötz Jensen <motz@e-s.dk>
2018-10-13 14:34:04 +0200

    Fix unit test

--- d365fo.tools/internal/functions/get-productinfoprovider.ps1
+++ d365fo.tools/internal/functions/get-productinfoprovider.ps1
@@ -1,5 +1,21 @@
-﻿function Get-ProductInfoProvider
-{
+﻿<#
+.SYNOPSIS
+Get the product information
+
+.DESCRIPTION
+Get the product information object from the environment
+
+.EXAMPLE
+PS C:\> Get-ProductInfoProvider
+
+This will get the product information object and return it
+
+.NOTES
+Author: Rasmus Andersen (@ITRasmus)
+Author: Mötz Jensen (@Splaxi)
+
+#>
+function Get-ProductInfoProvider {
     Add-Type -Path "$Script:AOSPath\bin\Microsoft.Dynamics.BusinessPlatform.ProductInformation.Provider.dll"
-    return [Microsoft.Dynamics.BusinessPlatform.ProductInformation.Provider.ProductInfoProvider]::get_Provider();
+    [Microsoft.Dynamics.BusinessPlatform.ProductInformation.Provider.ProductInfoProvider]::get_Provider()
 }
