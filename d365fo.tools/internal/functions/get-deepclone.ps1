1c89a7035b5bb0dfeba19f0957a5b3d7fe3c477f
Mötz Jensen <motz@e-s.dk>
2018-10-17 20:28:27 +0200

    Work on #85

--- d365fo.tools/internal/functions/get-deepclone.ps1
+++ d365fo.tools/internal/functions/get-deepclone.ps1
@@ -17,22 +17,26 @@ This will clone the $HashTable variable into a new object and return it to you.
 Author: Mötz Jensen (@Splaxi)
 
 #>
-function Get-DeepClone {
+function Get-DeepClone
+{
     [CmdletBinding()]
-    [OutputType('System.Collections.Hashtable')]
     param(
         [parameter(Mandatory = $true)]
-        [HashTable] $InputObject
+        $InputObject
     )
-    process {
-        if ($InputObject -is [hashtable]) {
+    process
+    {
+        if($InputObject -is [hashtable]) {
+
             $clone = @{}
-            foreach ($key in $InputObject.keys) {
+
+            foreach($key in $InputObject.keys)
+            {
                 $clone[$key] = Get-DeepClone $InputObject[$key]
             }
+
             $clone
-        }
-        else {
+        } else {
             $InputObject
         }
     }
