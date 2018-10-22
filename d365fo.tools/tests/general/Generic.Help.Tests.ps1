1fc7aba26009ad398bfbf457d3b1d2966dc46664
Mötz Jensen <motz@e-s.dk>
2018-10-22 22:22:51 +0200

    Update d365fo.tools/tests/general/Generic.Help.Tests.ps1

--- d365fo.tools/tests/general/Generic.Help.Tests.ps1
+++ d365fo.tools/tests/general/Generic.Help.Tests.ps1
@@ -9,8 +9,7 @@ $commandsRaw = Get-Command -Module d365fo.tools
 if ($excludeCommands.Count -gt 0) {
     $commands = $commandsRaw | Select-String -Pattern $excludeCommands -SimpleMatch -NotMatch
 
-}
-else {
+} else {
     $commands = $commandsRaw
 }
 
@@ -20,7 +19,6 @@ foreach ( $commandName in $commands) {
     #$commandName = 'Get-D365PackageLabelFile'
     # get all examples from the help
     $examples = Get-Help $commandName -Examples
-
 
     # make a describe block that will contain tests for this 
     Describe "Examples from $commandName" {
@@ -39,6 +37,29 @@ foreach ( $commandName in $commands) {
 
             if ( ($example -like "*|*" ) -or (-not ($example -match $commandName)) ) {
                 It "Example - $example" -Skip { $true }
+            } elseif ($example -like "*=*") {
+                $varAssignment = ($example -split "=")[0]
+
+                # for every example we want a single It block
+                It "Example - $example" {
+                    # mock the tested command so we don't actually do anything
+                    # because it can be unsafe and we don't have the environment setup
+                    # (so the only thing we are testing is that the code is semantically
+                    # correct and provides all the needed params)
+                    Mock $commandName { 
+                        # I am returning true here,
+                        # but some of the examples drill down to the returned object
+                        # so in strict mode we would fail
+                        $true 
+                    }
+
+                    $exampleExtended = "$example;$varAssignment"
+                    
+                    # here simply invoke the example
+                    $result = Invoke-Expression $exampleExtended
+                    # and check that we got result from the mock
+                    $result | Should -BeTrue
+                }
             } else {
                 # for every example we want a single It block
                 It "Example - $example" {
