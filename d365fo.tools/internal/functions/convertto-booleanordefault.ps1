200c66d822de8ddf59761865921b7c6bd798eae6
Mötz Jensen <motz@e-s.dk>
2018-10-23 23:19:07 +0200

    Update d365fo.tools/internal/functions/convertto-booleanordefault.ps1

--- d365fo.tools/internal/functions/convertto-booleanordefault.ps1
+++ d365fo.tools/internal/functions/convertto-booleanordefault.ps1
@@ -1,1 +1,36 @@
-﻿cmdlet
+﻿function ConvertTo-BooleanOrDefault {
+    [CmdletBinding()]
+    param (
+        [Object] $Object,
+
+        [Boolean] $Default
+    )
+
+    [boolean] $result = $Default;
+    $stringTrue = @("yes"    , "true"     , "ok"    , "y")
+
+    $stringFalse = @( "no", "false", "n"    )
+
+    try {
+        if (-not ($null -eq $Object) ) {
+            switch ($Object.ToString().ToLower()) {
+                {$stringTrue -contains $_} {
+                    $result = $true
+                    break
+                }
+                {$stringFalse-contains $_} {
+                    $result = $false
+                    break
+                }
+                default {
+                    $result = [System.Boolean]::Parser($Object.ToString())
+                    break
+                }
+            }
+        }
+    }
+    catch {
+    }
+
+    $result
+}
