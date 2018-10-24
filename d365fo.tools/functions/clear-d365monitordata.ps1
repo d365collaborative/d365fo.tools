e662a44e4c4e16b4fea2261609b331e60258c539
Mötz Jensen <motz@e-s.dk>
2018-10-24 21:37:12 +0200

    Update d365fo.tools/functions/clear-d365monitordata.ps1

---
+++ d365fo.tools/functions/clear-d365monitordata.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+Clear the monitoring data from a Dynamics 365 for Finance & Operations machine
+
+.DESCRIPTION
+Clear the monitoring data that is filling up the service drive on a Dynamics 365 for Finance & Operations
+
+.PARAMETER Path
+The path to where the monitoring data is located
+
+The default value is the "ServiceDrive" (j:\ | k:\) and the \MonAgentData\SingleAgent\Tables folder structure
+
+.EXAMPLE
+PS C:\> Clear-D365MonitorData
+
+This will delete all the files that are located in the default path on the machine.
+Some files might be locked by a process, but the cmdlet will attemp to delete all files.
+
+.NOTES
+Author: Mötz Jensen (@Splaxi)
+
+#>
+function Clear-D365MonitorData {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 1, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $true)]
+        [string] $Path = Join-Path ([System.Environment]::ExpandEnvironmentVariables("%ServiceDrive%")) "\MonAgentData\SingleAgent\Tables"
+    )
+    
+    Get-ChildItem -Path $Path | Remove-Item -Force -ErrorAction SilentlyContinue
+}
