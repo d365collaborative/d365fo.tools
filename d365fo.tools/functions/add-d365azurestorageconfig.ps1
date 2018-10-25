239adc1f92a07379f4bffef947a83c12b1a72310
Mötz Jensen <motz@e-s.dk>
2018-10-25 13:32:11 +0200

    Update d365fo.tools/functions/add-d365azurestorageconfig.ps1

--- d365fo.tools/functions/add-d365azurestorageconfig.ps1
+++ d365fo.tools/functions/add-d365azurestorageconfig.ps1
@@ -18,13 +18,31 @@
     .PARAMETER Blobname
         The name of the blob inside the Azure Storage Account you want to register in the configuration store
         
+    .PARAMETER ConfigStorageLocation
+        Parameter used to instruct where to store the configuration objects
+
+        The default value is "User" and this will store all configuration for the active user
+
+        Valid options are:
+        "User"
+        "System"
+
+        "System" will store the configuration so all users can access the configuration objects
+
     .PARAMETER Force
         Switch to instruct the cmdlet to overwrite already registered Azure Storage Account entry
         
     .EXAMPLE
         PS C:\> Add-D365AzureStorageConfig -Name "UAT-Exports" -AccountId "1234" -AccessToken "dafdfasdfasdf" -Blob "testblob"
         
-        This will add an entry into the list of Azure Storage Accounts that is stored with the name "UAT-Exports" with AccountId "1234", AccessToken "dafdfasdfasdf" and Blob "testblob"
+        This will add an entry into the list of Azure Storage Accounts that is stored with the name "UAT-Exports" with AccountId "1234", AccessToken "dafdfasdfasdf" and Blob "testblob".
+        
+    .EXAMPLE
+        PS C:\> Add-D365AzureStorageConfig -Name "UAT-Exports" -AccountId "1234" -AccessToken "dafdfasdfasdf" -Blob "testblob" -ConfigStorageLocation "System"
+
+        This will add an entry into the list of Azure Storage Accounts that is stored with the name "UAT-Exports" with AccountId "1234", AccessToken "dafdfasdfasdf" and Blob "testblob".
+        All configuration objects will be persisted in the system wide configuration store.
+        This will enable all users to access the configuration objects and their values.
         
     .NOTES
         
@@ -49,8 +67,24 @@ function Add-D365AzureStorageConfig {
         [Alias('Blob')]
         [string] $Blobname,
 
+        [ValidateSet('User', 'System')]
+        [string] $ConfigStorageLocation = "User",
+
         [switch] $Force
     )
+
+
+    $configScope = "UserDefault"
+
+    if ($ConfigStorageLocation -eq "System") {
+        if ($Script:IsAdminRuntime) {
+            $configScope = "SystemDefault"
+        }
+        else {
+            Write-PSFMessage -Level Host -Message "Unable to locate save the <c='em'>configuration objects</c> in the <c='em'>system wide configuration store</c> on the machine. Please start an elevated session and run the cmdlet again."
+            Stop-PSFFunction -Message "Elevated permissions needed. Please start an elevated session and run the cmdlet again."
+        }
+    }
 
     if ((Get-PSFConfig -FullName "d365fo.tools*").Count -eq 0) {
         Write-PSFMessage -Level Host -Message "Unable to locate the <c='em'>configuration objects</c> on the machine. Please make sure that you ran <c='em'>Initialize-D365Config</c> first."
@@ -64,7 +98,8 @@ function Add-D365AzureStorageConfig {
 
         $Accounts = [hashtable](Get-PSFConfigValue -FullName "d365fo.tools.azure.storage.accounts")
 
-        if(($null -eq $Accounts) -or ($Accounts.ContainsKey("Dummy"))) {$Accounts = @{}}
+        if (($null -eq $Accounts) -or ($Accounts.ContainsKey("Dummy"))) {$Accounts = @{}
+        }
         
         if ($Accounts.ContainsKey($Name)) {
             if ($Force.IsPresent) {
