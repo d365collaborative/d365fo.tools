4920c897a90f308b0c22fe1ee7af4da918c5a20b
Tommy Skaue <tommy_skaue@hotmail.com>
2018-11-02 21:45:31 +0100

    Update d365fo.tools/functions/import-d365bacpac.ps1
    More tests

--- d365fo.tools/functions/import-d365bacpac.ps1
+++ d365fo.tools/functions/import-d365bacpac.ps1
@@ -78,7 +78,7 @@
         It will import the "C:\temp\uat.bacpac" file into a new database named "ImportedDatabase".
         
     .EXAMPLE
-        PS C:\> Import-D365Bacpac -ImportModeTier2 -SqlUser "sqladmin" -SqlPwd "XyzXyz" -BacpacFile "C:\temp\uat.bacpac" -AxDeployExtUserPwd "XxXx" -AxDbAdminPwd "XxXx" -AxRuntimeUserPwd "XxXx" -AxMrRuntimeUserPwd "XxXx" -AxRetailRuntimeUserPwd "XxXx" -AxRetailDataSyncUserPwd "XxXx" -NewDatabaseName "ImportedDatabase"
+        PS C:\> Import-D365Bacpac -ImportModeTier2 -SqlUser "sqladmin" -SqlPwd "XyzXyz" -BacpacFile "C:\temp\uat.bacpac" -AxDeployExtUserPwd "XxXx" -AxDbAdminPwd "XxXx" -AxRuntimeUserPwd "XxXx" -AxMrRuntimeUserPwd "XxXx" -AxRetailRuntimeUserPwd "XxXx" -AxRetailDataSyncUserPwd "XxXx" -AxDbReadonlyUserPwd "XxXx" -NewDatabaseName "ImportedDatabase"
         
         This will instruct the cmdlet that the import will be working against an Azure DB instance.
         It requires all relevant passwords from LCS for all the builtin user accounts used in a Tier 2
