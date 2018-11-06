6dd230e2231d94a4d145e75ec8ecb8d7189d2fd0
Mötz Jensen <motz@e-s.dk>
2018-11-06 09:48:30 +0100

    Update build/vsts-prerequisites.ps1

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,14 +1,7 @@
-﻿Write-Host "Installing Pester" -ForegroundColor Cyan
-Install-Module Pester -Force -SkipPublisherCheck
-Write-Host "Installing PSFramework" -ForegroundColor Cyan
-Install-Module PSFramework -Force -SkipPublisherCheck
-Write-Host "Installing PSScriptAnalyzer" -ForegroundColor Cyan
-Install-Module PSScriptAnalyzer -Force -SkipPublisherCheck
-Write-Host "Installing Azure.Storage" -ForegroundColor Cyan
-Install-Module Azure.Storage -Force -SkipPublisherCheck
-Write-Host "Installing AzureAd" -ForegroundColor Cyan
-Install-Module AzureAd -Force -SkipPublisherCheck
-Write-Host "Installing PSNotification" -ForegroundColor Cyan
-Install-Module PSNotification -Force -SkipPublisherCheck
+﻿$modules = @("Pester", "PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification")
 
-Import-Module PSFramework -Force -PassThru -Verbose
+foreach ($module in $modules) {
+    Write-Host "Installing $module" -ForegroundColor Cyan
+    Install-Module $module -Force -SkipPublisherCheck
+    Import-Module $module -Force -PassThru
+}
