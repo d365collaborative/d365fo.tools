8e1fbf359c4facbaf422fd565386101de35a1264
Mötz Jensen <motz@e-s.dk>
2018-11-06 20:12:09 +0100

    Fixing

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,7 +1,12 @@
-﻿$modules = @("Pester", "PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification")
+﻿Write-Host "Working on the machine named: $($env:computername)"
+Write-Host "The user running is: $($env:UserName)"
+
+$modules = @("Pester", "PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification")
 
 foreach ($module in $modules) {
     Write-Host "Installing $module" -ForegroundColor Cyan
     Install-Module $module -Force -SkipPublisherCheck -Scope AllUsers
     Import-Module $module -Force -PassThru
 }
+
+(Get-Module -ListAvailable).ModuleBase
