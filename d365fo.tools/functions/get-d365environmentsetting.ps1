67fda483148a5c1dd8c3a33db81f1b7277ac25b0
Mötz Jensen <motz@e-s.dk>
2018-11-22 11:12:28 +0100

    Update get-d365environmentsetting.ps1

--- d365fo.tools/functions/get-d365environmentsetting.ps1
+++ d365fo.tools/functions/get-d365environmentsetting.ps1
@@ -7,12 +7,12 @@
         Gets all settings the Dynamics 365 for Finance & Operations environment uses.
         
     .EXAMPLE
-        PS C:\> Get-D365EnvironmentSetting
+        PS C:\> Get-D365EnvironmentSettings
         
         This will get all details available for the environment
         
     .EXAMPLE
-        PS C:\> Get-D365EnvironmentSetting | Format-Custom -Property *
+        PS C:\> Get-D365EnvironmentSettings | Format-Custom -Property *
         
         This will get all details available for the environment and format it to show all details in a long custom object.
         
@@ -20,6 +20,7 @@
         Tags: Environment, Configuration, WebConfig, Web.Config, Decryption
         
         Author: Rasmus Andersen (@ITRasmus)
+        Author: Mötz Jensen (@Splaxi)
         
         The cmdlet wraps the call against a dll file that is shipped with Dynamics 365 for Finance & Operations.
         The call to the dll file gets all relevant details for the installation.
