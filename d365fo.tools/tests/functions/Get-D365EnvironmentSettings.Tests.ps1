42b8249d63dd4a7e90127c2f7af718feb2483371
Mötz Jensen <motz@e-s.dk>
2018-11-22 11:14:26 +0100

    Create Get-D365EnvironmentSettings.Tests.ps1

---
+++ d365fo.tools/tests/functions/Get-D365EnvironmentSettings.Tests.ps1
@@ -0,0 +1,24 @@
+﻿Describe "Get-D365EnvironmentSettings Unit Tests" -Tag "Unit" {
+	BeforeAll {
+		# Place here all things needed to prepare for the tests
+	}
+	AfterAll {
+		# Here is where all the cleanup tasks go
+	}
+	
+	Describe "Ensuring unchanged command signature" {
+		It "should have the expected parameter sets" {
+			(Get-Command Get-D365EnvironmentSettings).ParameterSets.Name | Should -Be '__AllParameterSets'
+		}
+		
+
+	}
+	
+	Describe "Testing parameterset __AllParameterSets" {
+		<#
+		__AllParameterSets -
+		__AllParameterSets -
+		#>
+	}
+
+}
