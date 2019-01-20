f0e0612629fce21ecb276459f7e7fbf00c80df8b
Mötz Jensen <motz@e-s.dk>
2019-01-21 00:21:16 +0100

    Update get-d365lcsuploadconfig.ps1

--- d365fo.tools/functions/get-d365lcsuploadconfig.ps1
+++ d365fo.tools/functions/get-d365lcsuploadconfig.ps1
@@ -1,4 +1,39 @@
-﻿function Get-D365LcsUploadConfig {
+﻿<#
+.SYNOPSIS
+Get the LCS configuration details
+
+.DESCRIPTION
+Get the LCS configuration details from the configuration store
+
+.PARAMETER OutputType
+The output type you want the cmdlet to return to you
+
+Default value is "HashTable"
+
+Valid options:
+HashTable
+PSCustomObject
+
+.EXAMPLE
+PS C:\> Get-D365LcsUploadConfig
+
+This will return the saved configuration for accessing the LCS API.
+The object return will be a HashTable.
+
+.EXAMPLE
+PS C:\> Get-D365LcsUploadConfig -OutputType "PSCustomObject"
+
+This will return the saved configuration for accessing the LCS API.
+The object return will be a PSCustomObject.
+
+.NOTES
+Tags: Environment, Url, Config, Configuration, LCS, Upload, ClientId
+
+Author: Mötz Jensen (@Splaxi)
+
+#>
+
+function Get-D365LcsUploadConfig {
     [CmdletBinding()]
     [OutputType()]
     param (
@@ -8,6 +43,8 @@
     )
 
     $res = [Ordered]@{}
+
+    Write-PSFMessage -Level Verbose -Message "Extracting all the LCS configuration and building the result object."
 
     foreach ($item in (Get-PSFConfig -FullName d365fo.tools.lcs*)) {
         $nameTemp = $item.FullName -replace "^d365fo.tools.lcs.upload.", ""
