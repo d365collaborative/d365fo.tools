e532f234a6b87f264d3151674683269a62231277
Mötz Jensen <motz@e-s.dk>
2019-01-21 00:02:42 +0100

    Update convert-hashtoargstringswitch.ps1

--- d365fo.tools/internal/functions/convert-hashtoargstringswitch.ps1
+++ d365fo.tools/internal/functions/convert-hashtoargstringswitch.ps1
@@ -1,5 +1,4 @@
-﻿
-<#
+﻿<#
     .SYNOPSIS
         Convert HashTable into an array
         
@@ -21,16 +20,30 @@
         
         The default value is ":"
         
+    .PARAMETER KeepCase
+        Instruct the cmdlet to keep the naming case of the properties from the hashtable
+
+        Default value is: $true
+
     .EXAMPLE
         PS C:\> $params = @{NoPrompt = $true; CreateParents = $false}
         PS C:\> $arguments = Convert-HashToArgStringSwitch -Inputs $params
         
-        This will convert the $params into an array of strings, each with the Key:Value.
+        This will convert the $params into an array of strings, each with the "-Key:Value" pattern.
+        
+        .EXAMPLE
+        PS C:\> $params = @{NoPrompt = $true; CreateParents = $false}
+        PS C:\> $arguments = Convert-HashToArgStringSwitch -InputObject $params -KeyPrefix "&" -ValuePrefix "="
+        
+        This will convert the $params into an array of strings, each with the "&Key=Value" pattern.
         
     .NOTES
-        Author: Mötz Jensen (@Splaxi)
+    Tags: HashTable, Arguments
+    
+            Author: Mötz Jensen (@Splaxi)
         
 #>
+
 function Convert-HashToArgStringSwitch {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidDefaultValueSwitchParameter", "")]
     [CmdletBinding()]
@@ -47,7 +60,7 @@ function Convert-HashToArgStringSwitch {
 
     foreach ($key in $InputObject.Keys) {
         $value = "{0}" -f $InputObject.Item($key).ToString()
-        if(-not $KeepCase) {$value = $value.ToLower()}
+        if (-not $KeepCase) {$value = $value.ToLower()}
         "$KeyPrefix$($key)$ValuePrefix$($value)"
     }
 }
