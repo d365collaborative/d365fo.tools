6a545dfae37eeff33a15403accce68dd10a8ec33
Mötz Jensen <motz@e-s.dk>
2019-01-21 00:14:00 +0100

    Update get-asyncresult.ps1

--- d365fo.tools/internal/functions/get-asyncresult.ps1
+++ d365fo.tools/internal/functions/get-asyncresult.ps1
@@ -1,4 +1,26 @@
-﻿function Get-AsyncResult {
+﻿<#
+.SYNOPSIS
+Simple abstraction to handle asynchronous executions
+
+.DESCRIPTION
+Simple abstraction to handle asynchronous executions for several other cmdlets
+
+.PARAMETER Task
+The task you want to work / wait for to complete
+
+.EXAMPLE
+PS C:\> Get-AsyncResult -Task $client.SendAsync($request)
+
+This will take the client (http) and have it send a request using the asynchronous pattern.
+
+.NOTES
+Tags: Async, Waiter, Wait
+
+Author: Mötz Jensen (@Splaxi)
+
+#>
+
+function Get-AsyncResult {
     [CmdletBinding()]
     [OutputType('Object')]
     param (
@@ -6,5 +28,6 @@
         [object] $Task
     )
 
+    Write-PSFMessage -Level Verbose -Message "Building the Task Waiter and start waiting." -Target $Task
     $Task.GetAwaiter().GetResult()
 }
