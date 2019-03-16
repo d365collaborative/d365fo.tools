82dc08581019a4aca81c4a9ddd4dbee558426187
Mötz Jensen <motz@e-s.dk>
2019-03-16 13:58:29 +0100

    Update get-d365azurestoragefile.ps1

--- d365fo.tools/functions/get-d365azurestoragefile.ps1
+++ d365fo.tools/functions/get-d365azurestoragefile.ps1
@@ -11,7 +11,10 @@
         
     .PARAMETER AccessToken
         The token that has the needed permissions for the search action
-        
+
+    .PARAMETER SAS
+        The SAS key that you have created for the storage account or blob container
+
     .PARAMETER Container
         Name of the blob container inside the storage account you want to look for files
         
@@ -28,12 +31,26 @@
     .EXAMPLE
         PS C:\> Get-D365AzureStorageFile -AccountId "miscfiles" -AccessToken "xx508xx63817x752xx74004x30705xx92x58349x5x78f5xx34xxxxx51" -Container "backupfiles"
         
-        Will get all files in the blob container
+        This will get all files in the blob container "backupfiles".
+        It will use the AccessToken "xx508xx63817x752xx74004x30705xx92x58349x5x78f5xx34xxxxx51" to gain access.
         
+    .EXAMPLE
+        PS C:\> Get-D365AzureStorageFile -AccountId "miscfiles" -AccessToken "xx508xx63817x752xx74004x30705xx92x58349x5x78f5xx34xxxxx51" -Container "backupfiles" -Latest
+        
+        This will get the latest (newest) file from the blob container "backupfiles".
+        It will use the AccessToken "xx508xx63817x752xx74004x30705xx92x58349x5x78f5xx34xxxxx51" to gain access to the container.
+
     .EXAMPLE
         PS C:\> Get-D365AzureStorageFile -AccountId "miscfiles" -AccessToken "xx508xx63817x752xx74004x30705xx92x58349x5x78f5xx34xxxxx51" -Container "backupfiles" -Name "*UAT*"
         
-        Will get all files in the blob container that fits the "*UAT*" search value
+        This will get all files in the blob container "backupfiles" that fits the "*UAT*" search value.
+        It will use the AccessToken "xx508xx63817x752xx74004x30705xx92x58349x5x78f5xx34xxxxx51" to gain access to the container.
+
+    .EXAMPLE
+        PS C:\> Get-D365AzureStorageFile -AccountId "miscfiles" -SAS "sv2018-03-28&siunlisted&src&sigAUOpdsfpoWE976ASDhfjkasdf(5678sdfhk" -Container "backupfiles" -Latest
+        
+        This will get the latest (newest) file from the blob container "backupfiles".
+        It will use the SAS key "sv2018-03-28&siunlisted&src&sigAUOpdsfpoWE976ASDhfjkasdf(5678sdfhk" to gain access to the container.
         
     .NOTES
         Tags: Azure, Azure Storage, Token, Blob, File, Container
