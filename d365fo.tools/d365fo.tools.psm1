492898bd3a6aaaf04bb28add52d809feb074e705
Mötz Jensen <motz@e-s.dk>
2019-03-28 15:37:49 +0100

    Update module version

--- d365fo.tools/d365fo.tools.psm1
+++ d365fo.tools/d365fo.tools.psm1
@@ -1,5 +1,5 @@
 ﻿$script:ModuleRoot = $PSScriptRoot
-$script:ModuleVersion = "0.5.17"
+$script:ModuleVersion = "0.5.18"
 
 # Detect whether at some level dotsourcing was enforced
 $script:doDotSource = Get-PSFConfigValue -FullName d365fo.tools.Import.DoDotSource -Fallback $false
