70b638449e09580996075c7ada156549b0dfbe09
Mötz Jensen <motz@e-s.dk>
2019-04-29 12:31:29 +0200

    Update send-d365broadcastmessage.ps1

--- d365fo.tools/functions/send-d365broadcastmessage.ps1
+++ d365fo.tools/functions/send-d365broadcastmessage.ps1
@@ -6,16 +6,12 @@
     .DESCRIPTION
         Utilize the same messaging framework available from LCS and send a broadcast message to all online users in the environment
         
-    .PARAMETER AuthProviderUri
-        The URL / URI for the authentication provider that you want to authenticate against
-        
-        Default is https://login.microsoftonline.com/[TENANTID]/oauth2/token
-        
-        [TENANTID] is the GUID from your Azure Active Directory then D365FO is connected to
+    .PARAMETER Tenant
+        Azure Active Directory (AAD) tenant id (Guid) that the D365FO environment is connected to, that you want to send a message to
         
     .PARAMETER URL
         URL / URI for the D365FO environment you want to send a message to
-        
+
     .PARAMETER ClientId
         The ClientId obtained from the Azure Portal when you created a Registered Application
         
@@ -46,7 +42,7 @@
         The specified StartTime will always be based on local Time Zone. If you specify a different Time Zone than the local computer is running, the start and end time will be calculated based on your selection.
         
     .EXAMPLE
-        PS C:\> Send-D365BroadcastMessage -AuthProviderUri "https://login.microsoftonline.com/e674da86-7ee5-40a7-b777-1111111111111/oauth2/token" -URL "https://usnconeboxax1aos.cloud.onebox.dynamics.com" -ClientId "dea8d7a9-1602-4429-b138-111111111111" -ClientSecret "Vja/VmdxaLOPR+alkjfsadffelkjlfw234522="
+        PS C:\> Send-D365BroadcastMessage -Tenant "e674da86-7ee5-40a7-b777-1111111111111" -URL "https://usnconeboxax1aos.cloud.onebox.dynamics.com" -ClientId "dea8d7a9-1602-4429-b138-111111111111" -ClientSecret "Vja/VmdxaLOPR+alkjfsadffelkjlfw234522="
         
         This will send a message to all active users that are working on the D365FO environment located at "https://usnconeboxax1aos.cloud.onebox.dynamics.com".
         It will authenticate against the Azure Active Directory with the "e674da86-7ee5-40a7-b777-1111111111111" GUID.
@@ -70,7 +66,8 @@ function Send-D365BroadcastMessage {
     [OutputType()]
     param (
         [Parameter(Mandatory = $true, Position = 1)]
-        [string] $AuthProviderUri,
+        [Alias('$AADGuid')]
+        [string] $Tenant,
 
         [Parameter(Mandatory = $true, Position = 2)]
         [Alias('URI')]
@@ -91,9 +88,9 @@ function Send-D365BroadcastMessage {
         [Parameter(Mandatory = $false, Position = 7)]
         [int] $EndingInMinutes = 60
     )
-    
+
     $bearerParms = @{
-        AuthProviderUri = $AuthProviderUri
+        AuthProviderUri = "https://login.microsoftonline.com/$Tenant/oauth2/token"
         Resource        = $URL
         ClientId        = $ClientId
         ClientSecret    = $ClientSecret
