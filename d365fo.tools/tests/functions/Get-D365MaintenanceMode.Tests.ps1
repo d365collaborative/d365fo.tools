f3a28cf54c8b165f95083631667f374b8e4394b4
Mötz Jensen <motz@e-s.dk>
2019-04-30 09:49:20 +0200

    Pester updated

---
+++ d365fo.tools/tests/functions/Get-D365MaintenanceMode.Tests.ps1
@@ -0,0 +1,75 @@
+﻿Describe "Get-D365MaintenanceMode Unit Tests" -Tag "Unit" {
+	BeforeAll {
+		# Place here all things needed to prepare for the tests
+	}
+	AfterAll {
+		# Here is where all the cleanup tasks go
+	}
+	
+	Describe "Ensuring unchanged command signature" {
+		It "should have the expected parameter sets" {
+			(Get-Command Get-D365MaintenanceMode).ParameterSets.Name | Should -Be 'Default'
+		}
+		
+		It 'Should have the expected parameter DatabaseServer' {
+			$parameter = (Get-Command Get-D365MaintenanceMode).Parameters['DatabaseServer']
+			$parameter.Name | Should -Be 'DatabaseServer'
+			$parameter.ParameterType.ToString() | Should -Be System.String
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be 'Default'
+			$parameter.ParameterSets.Keys | Should -Contain 'Default'
+			$parameter.ParameterSets['Default'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['Default'].Position | Should -Be 3
+			$parameter.ParameterSets['Default'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromRemainingArguments | Should -Be $False
+		}
+		It 'Should have the expected parameter DatabaseName' {
+			$parameter = (Get-Command Get-D365MaintenanceMode).Parameters['DatabaseName']
+			$parameter.Name | Should -Be 'DatabaseName'
+			$parameter.ParameterType.ToString() | Should -Be System.String
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be 'Default'
+			$parameter.ParameterSets.Keys | Should -Contain 'Default'
+			$parameter.ParameterSets['Default'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['Default'].Position | Should -Be 4
+			$parameter.ParameterSets['Default'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromRemainingArguments | Should -Be $False
+		}
+		It 'Should have the expected parameter SqlUser' {
+			$parameter = (Get-Command Get-D365MaintenanceMode).Parameters['SqlUser']
+			$parameter.Name | Should -Be 'SqlUser'
+			$parameter.ParameterType.ToString() | Should -Be System.String
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be 'Default'
+			$parameter.ParameterSets.Keys | Should -Contain 'Default'
+			$parameter.ParameterSets['Default'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['Default'].Position | Should -Be 5
+			$parameter.ParameterSets['Default'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromRemainingArguments | Should -Be $False
+		}
+		It 'Should have the expected parameter SqlPwd' {
+			$parameter = (Get-Command Get-D365MaintenanceMode).Parameters['SqlPwd']
+			$parameter.Name | Should -Be 'SqlPwd'
+			$parameter.ParameterType.ToString() | Should -Be System.String
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be 'Default'
+			$parameter.ParameterSets.Keys | Should -Contain 'Default'
+			$parameter.ParameterSets['Default'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['Default'].Position | Should -Be 6
+			$parameter.ParameterSets['Default'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['Default'].ValueFromRemainingArguments | Should -Be $False
+		}
+	}
+	
+	Describe "Testing parameterset Default" {
+		<#
+		Default -
+		Default -DatabaseServer -DatabaseName -SqlUser -SqlPwd
+		#>
+	}
+
+}
