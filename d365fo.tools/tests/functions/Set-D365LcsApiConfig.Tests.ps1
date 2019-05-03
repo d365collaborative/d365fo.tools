d9296a972c564522c50bf6824c91499b40f00d23
Mötz Jensen <motz@e-s.dk>
2019-05-03 20:27:49 +0200

    Pester updated

--- d365fo.tools/tests/functions/Set-D365LcsApiConfig.Tests.ps1
+++ d365fo.tools/tests/functions/Set-D365LcsApiConfig.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Describe "Invoke-D365LcsUpload Unit Tests" -Tag "Unit" {
+﻿Describe "Set-D365LcsApiConfig Unit Tests" -Tag "Unit" {
 	BeforeAll {
 		# Place here all things needed to prepare for the tests
 	}
@@ -8,11 +8,11 @@
 	
 	Describe "Ensuring unchanged command signature" {
 		It "should have the expected parameter sets" {
-			(Get-Command Invoke-D365LcsUpload).ParameterSets.Name | Should -Be '__AllParameterSets'
+			(Get-Command Set-D365LcsApiConfig).ParameterSets.Name | Should -Be '__AllParameterSets'
 		}
 		
 		It 'Should have the expected parameter ProjectId' {
-			$parameter = (Get-Command Invoke-D365LcsUpload).Parameters['ProjectId']
+			$parameter = (Get-Command Set-D365LcsApiConfig).Parameters['ProjectId']
 			$parameter.Name | Should -Be 'ProjectId'
 			$parameter.ParameterType.ToString() | Should -Be System.Int32
 			$parameter.IsDynamic | Should -Be $False
@@ -24,9 +24,9 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter BearerToken' {
-			$parameter = (Get-Command Invoke-D365LcsUpload).Parameters['BearerToken']
-			$parameter.Name | Should -Be 'BearerToken'
+		It 'Should have the expected parameter ClientId' {
+			$parameter = (Get-Command Set-D365LcsApiConfig).Parameters['ClientId']
+			$parameter.Name | Should -Be 'ClientId'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
@@ -37,22 +37,48 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter FilePath' {
-			$parameter = (Get-Command Invoke-D365LcsUpload).Parameters['FilePath']
-			$parameter.Name | Should -Be 'FilePath'
+		It 'Should have the expected parameter BearerToken' {
+			$parameter = (Get-Command Set-D365LcsApiConfig).Parameters['BearerToken']
+			$parameter.Name | Should -Be 'BearerToken'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
 			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
-			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $True
-			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 5
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 3
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $True
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter FileType' {
-			$parameter = (Get-Command Invoke-D365LcsUpload).Parameters['FileType']
-			$parameter.Name | Should -Be 'FileType'
+		It 'Should have the expected parameter ActiveTokenExpiresOn' {
+			$parameter = (Get-Command Set-D365LcsApiConfig).Parameters['ActiveTokenExpiresOn']
+			$parameter.Name | Should -Be 'ActiveTokenExpiresOn'
+			$parameter.ParameterType.ToString() | Should -Be System.Int64
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 4
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $True
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
+		It 'Should have the expected parameter RefreshToken' {
+			$parameter = (Get-Command Set-D365LcsApiConfig).Parameters['RefreshToken']
+			$parameter.Name | Should -Be 'RefreshToken'
+			$parameter.ParameterType.ToString() | Should -Be System.String
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 5
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $True
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
+		It 'Should have the expected parameter LcsApiUri' {
+			$parameter = (Get-Command Set-D365LcsApiConfig).Parameters['LcsApiUri']
+			$parameter.Name | Should -Be 'LcsApiUri'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
@@ -60,44 +86,18 @@
 			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 6
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $True
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter FileName' {
-			$parameter = (Get-Command Invoke-D365LcsUpload).Parameters['FileName']
-			$parameter.Name | Should -Be 'FileName'
-			$parameter.ParameterType.ToString() | Should -Be System.String
+		It 'Should have the expected parameter Temporary' {
+			$parameter = (Get-Command Set-D365LcsApiConfig).Parameters['Temporary']
+			$parameter.Name | Should -Be 'Temporary'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
 			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
 			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 7
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
-		}
-		It 'Should have the expected parameter FileDescription' {
-			$parameter = (Get-Command Invoke-D365LcsUpload).Parameters['FileDescription']
-			$parameter.Name | Should -Be 'FileDescription'
-			$parameter.ParameterType.ToString() | Should -Be System.String
-			$parameter.IsDynamic | Should -Be $False
-			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
-			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
-			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 8
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
-		}
-		It 'Should have the expected parameter LcsApiUri' {
-			$parameter = (Get-Command Invoke-D365LcsUpload).Parameters['LcsApiUri']
-			$parameter.Name | Should -Be 'LcsApiUri'
-			$parameter.ParameterType.ToString() | Should -Be System.String
-			$parameter.IsDynamic | Should -Be $False
-			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
-			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
-			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 9
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
@@ -106,8 +106,8 @@
 	
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
-		__AllParameterSets -FilePath
-		__AllParameterSets -ProjectId -BearerToken -FilePath -FileType -FileName -FileDescription -LcsApiUri
+		__AllParameterSets -
+		__AllParameterSets -ProjectId -ClientId -BearerToken -ActiveTokenExpiresOn -RefreshToken -LcsApiUri -Temporary
 		#>
 	}
 
