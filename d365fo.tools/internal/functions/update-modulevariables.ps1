24345ead7ea91b9b778193df38df1559ac5b63cc
Mötz Jensen <motz@e-s.dk>
2019-05-03 20:41:31 +0200

    Update update-modulevariables.ps1

--- d365fo.tools/internal/functions/update-modulevariables.ps1
+++ d365fo.tools/internal/functions/update-modulevariables.ps1
@@ -14,6 +14,7 @@
 #>
 
 function Update-ModuleVariables {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
     [CmdletBinding()]
     [OutputType()]
     param ( )
