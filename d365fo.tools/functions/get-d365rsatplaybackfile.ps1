a42e7ac51e4a3777bf945da81aed22edd9ac9b44
Mötz Jensen <motz@e-s.dk>
2019-05-16 08:45:32 +0200

    Update get-d365rsatplaybackfile.ps1

--- d365fo.tools/functions/get-d365rsatplaybackfile.ps1
+++ d365fo.tools/functions/get-d365rsatplaybackfile.ps1
@@ -1,46 +1,47 @@
-﻿<#
-.SYNOPSIS
-Get the RSAT playback files
-
-.DESCRIPTION
-Get all the RSAT playback files from the last executions
-
-.PARAMETER Path
-The path where the RSAT tool will be writing the files
-
-The default path is:
-"C:\Users\USERNAME\AppData\Roaming\regressionTool\playback"
-
-.PARAMETER Name
-Name of Test Case that you are looking for
-
-Default value is "*" which will search for all Test Cases and their corresponding files
-
-.PARAMETER ExecutionUsername
-Name of the user account has been running the RSAT tests on a machine that isn't the same as the current user
-
-Will enable you to log on to RSAT server that is running the tests from a console, automated, and is other account than the current user
-
-.EXAMPLE
-PS C:\> Get-D365RsatPlaybackFile
-
-This will get all the RSAT playback files.
-It will search for the files in the current user AppData system folder.
-
-.EXAMPLE
-PS C:\> Get-D365RsatPlaybackFile -Name *4080*
-
-This will get all the RSAT playback files which has "4080" as part of its name.
-It will search for the files in the current user AppData system folder.
-
-.EXAMPLE
-PS C:\> Get-D365RsatPlaybackFile -ExecutionUsername RSAT-ServiceAccount
-
-This will get all the RSAT playback files that were executed by the RSAT-ServiceAccount user.
-It will search for the files in the RSAT-ServiceAccount user AppData system folder.
-
-.NOTES
-Tags: RSAT, Testing, Regression Suite Automation Test, Regression, Test, Automation, Playback
+﻿
+<#
+    .SYNOPSIS
+        Get the RSAT playback files
+        
+    .DESCRIPTION
+        Get all the RSAT playback files from the last executions
+        
+    .PARAMETER Path
+        The path where the RSAT tool will be writing the files
+        
+        The default path is:
+        "C:\Users\USERNAME\AppData\Roaming\regressionTool\playback"
+        
+    .PARAMETER Name
+        Name of Test Case that you are looking for
+        
+        Default value is "*" which will search for all Test Cases and their corresponding files
+        
+    .PARAMETER ExecutionUsername
+        Name of the user account has been running the RSAT tests on a machine that isn't the same as the current user
+        
+        Will enable you to log on to RSAT server that is running the tests from a console, automated, and is other account than the current user
+        
+    .EXAMPLE
+        PS C:\> Get-D365RsatPlaybackFile
+        
+        This will get all the RSAT playback files.
+        It will search for the files in the current user AppData system folder.
+        
+    .EXAMPLE
+        PS C:\> Get-D365RsatPlaybackFile -Name *4080*
+        
+        This will get all the RSAT playback files which has "4080" as part of its name.
+        It will search for the files in the current user AppData system folder.
+        
+    .EXAMPLE
+        PS C:\> Get-D365RsatPlaybackFile -ExecutionUsername RSAT-ServiceAccount
+        
+        This will get all the RSAT playback files that were executed by the RSAT-ServiceAccount user.
+        It will search for the files in the RSAT-ServiceAccount user AppData system folder.
+        
+    .NOTES
+        Tags: RSAT, Testing, Regression Suite Automation Test, Regression, Test, Automation, Playback
         
         Author: Mötz Jensen (@Splaxi)
 #>
