052a2fd023f4f56ac9d1d7eaa16de6de4a852c2c
Mötz Jensen <motz@e-s.dk>
2019-10-03 08:14:21 +0200

    Import

--- d365fo.tools/tests/functions/Get-D365LcsDatabaseRefreshStatus.Tests.ps1
+++ d365fo.tools/tests/functions/Get-D365LcsDatabaseRefreshStatus.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Describe "Invoke-D365LcsDatabaseRefresh Unit Tests" -Tag "Unit" {
+﻿Describe "Get-D365LcsDatabaseRefreshStatus Unit Tests" -Tag "Unit" {
 	BeforeAll {
 		# Place here all things needed to prepare for the tests
 	}
@@ -8,11 +8,11 @@
 	
 	Describe "Ensuring unchanged command signature" {
 		It "should have the expected parameter sets" {
-			(Get-Command Invoke-D365LcsDatabaseRefresh).ParameterSets.Name | Should -Be '__AllParameterSets'
+			(Get-Command Get-D365LcsDatabaseRefreshStatus).ParameterSets.Name | Should -Be '__AllParameterSets'
 		}
 		
 		It 'Should have the expected parameter ProjectId' {
-			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['ProjectId']
+			$parameter = (Get-Command Get-D365LcsDatabaseRefreshStatus).Parameters['ProjectId']
 			$parameter.Name | Should -Be 'ProjectId'
 			$parameter.ParameterType.ToString() | Should -Be System.Int32
 			$parameter.IsDynamic | Should -Be $False
@@ -25,7 +25,7 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
 		It 'Should have the expected parameter BearerToken' {
-			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['BearerToken']
+			$parameter = (Get-Command Get-D365LcsDatabaseRefreshStatus).Parameters['BearerToken']
 			$parameter.Name | Should -Be 'BearerToken'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
@@ -37,9 +37,9 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter SourceEnvironmentId' {
-			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['SourceEnvironmentId']
-			$parameter.Name | Should -Be 'SourceEnvironmentId'
+		It 'Should have the expected parameter OperationActivityId' {
+			$parameter = (Get-Command Get-D365LcsDatabaseRefreshStatus).Parameters['OperationActivityId']
+			$parameter.Name | Should -Be 'OperationActivityId'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
@@ -47,12 +47,12 @@
 			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $True
 			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 2
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $True
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter TargetEnvironmentId' {
-			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['TargetEnvironmentId']
-			$parameter.Name | Should -Be 'TargetEnvironmentId'
+		It 'Should have the expected parameter EnvironmentId' {
+			$parameter = (Get-Command Get-D365LcsDatabaseRefreshStatus).Parameters['EnvironmentId']
+			$parameter.Name | Should -Be 'EnvironmentId'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
@@ -64,7 +64,7 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
 		It 'Should have the expected parameter LcsApiUri' {
-			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['LcsApiUri']
+			$parameter = (Get-Command Get-D365LcsDatabaseRefreshStatus).Parameters['LcsApiUri']
 			$parameter.Name | Should -Be 'LcsApiUri'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
@@ -76,9 +76,9 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter SkipInitialStatusFetch' {
-			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['SkipInitialStatusFetch']
-			$parameter.Name | Should -Be 'SkipInitialStatusFetch'
+		It 'Should have the expected parameter WaitForCompletion' {
+			$parameter = (Get-Command Get-D365LcsDatabaseRefreshStatus).Parameters['WaitForCompletion']
+			$parameter.Name | Should -Be 'WaitForCompletion'
 			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
@@ -89,12 +89,25 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter SleepInSeconds' {
+			$parameter = (Get-Command Get-D365LcsDatabaseRefreshStatus).Parameters['SleepInSeconds']
+			$parameter.Name | Should -Be 'SleepInSeconds'
+			$parameter.ParameterType.ToString() | Should -Be System.Int32
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 5
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 	}
 	
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
-		__AllParameterSets -SourceEnvironmentId -TargetEnvironmentId
-		__AllParameterSets -ProjectId -BearerToken -SourceEnvironmentId -TargetEnvironmentId -LcsApiUri -SkipInitialStatusFetch
+		__AllParameterSets -OperationActivityId -EnvironmentId
+		__AllParameterSets -ProjectId -BearerToken -OperationActivityId -EnvironmentId -LcsApiUri -WaitForCompletion -SleepInSeconds
 		#>
 	}
 
--- d365fo.tools/tests/functions/Get-D365LcsDeploymentStatus.Tests.ps1
+++ d365fo.tools/tests/functions/Get-D365LcsDeploymentStatus.Tests.ps1
@@ -89,12 +89,25 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter SleepInSeconds' {
+			$parameter = (Get-Command Get-D365LcsDeploymentStatus).Parameters['SleepInSeconds']
+			$parameter.Name | Should -Be 'SleepInSeconds'
+			$parameter.ParameterType.ToString() | Should -Be System.Int32
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 5
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 	}
 	
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -ActionHistoryId -EnvironmentId
-		__AllParameterSets -ProjectId -BearerToken -ActionHistoryId -EnvironmentId -LcsApiUri -WaitForCompletion
+		__AllParameterSets -ProjectId -BearerToken -ActionHistoryId -EnvironmentId -LcsApiUri -WaitForCompletion -SleepInSeconds
 		#>
 	}
 
