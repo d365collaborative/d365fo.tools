bd4f1d9bc6ce4a841ce4e08c84601336551fe3d6
Mötz Jensen <motz@e-s.dk>
2019-12-17 07:26:48 +0100

    Update clear-d365tabledatafrombacpac.ps1

--- d365fo.tools/functions/clear-d365tabledatafrombacpac.ps1
+++ d365fo.tools/functions/clear-d365tabledatafrombacpac.ps1
@@ -18,14 +18,26 @@
     )
     
     begin {
+        $originalExtension = ""
+
         $fileName = [System.IO.Path]::GetFileNameWithoutExtension($Path)
+        
+        if($Path -like "*.bacpac") {
+            Write-PSFMessage -Level Verbose -Message "Renaming the bacpac file to zip, to be able to extract the file." -Target $Path
+
+            Rename-Item -Path $Path -NewName "$($fileName).zip"
+
+            $originalExtension = "bacpac½"
+        }
+
         $workPath = Join-Path -Path $ExtractionPath -ChildPath $fileName
+        $archivePath = Join-Path -Path $ExtractionPath -ChildPath "$($fileName).zip"
 
         if (-not (Test-PathExists -Path $ExtractionPath,$workPath -Type Container -Create)) { return }
 
         if (-not (Test-PathExists -Path $File -Type Leaf)) { return }
 
-        Expand-Archive -Path $Path -DestinationPath $workPath -Force
+        Expand-Archive -Path $archivePath -DestinationPath $workPath -Force
     }
     
     process {
@@ -38,6 +50,9 @@
     }
     
     end {
-        
+        if($originalExtension -eq "bacpac½"){
+            Rename-Item -Path $archivePath -NewName "$($fileName).bacpac"
+
+        }
     }
 }
