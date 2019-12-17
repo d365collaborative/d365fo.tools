58497266b8f2477122e2ad7eeff173463fa0d202
Mötz Jensen <motz@e-s.dk>
2019-12-17 07:02:53 +0100

    Create clear-d365tabledatafrombacpac.ps1

---
+++ d365fo.tools/functions/clear-d365tabledatafrombacpac.ps1
@@ -0,0 +1,43 @@
+﻿function Clear-D365TableDataFromBacpac {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Alias('File')]
+        [string] $Path,
+
+        [Parameter(Mandatory = $true)]
+        [string[]] $TableName,
+
+        [Parameter(Mandatory = $true)]
+        [string] $OutputPath,
+
+        [string] $ExtractionPath = $(Join-Path $Script:DefaultTempPath "BacpacExtractions"),
+
+        [switch] $KeepFiles
+
+    )
+    
+    begin {
+        $fileName = [System.IO.Path]::GetFileNameWithoutExtension($Path)
+        $workPath = Join-Path -Path $ExtractionPath -ChildPath $fileName
+
+        if (-not (Test-PathExists -Path $ExtractionPath,$workPath -Type Container -Create)) { return }
+
+        if (-not (Test-PathExists -Path $File -Type Leaf)) { return }
+
+        Expand-Archive -Path $Path -DestinationPath $workPath -Force
+    }
+    
+    process {
+        if (Test-PSFFunctionInterrupt) { return }
+        
+        foreach ($table in $TableName) {
+            
+        }
+
+    }
+    
+    end {
+        
+    }
+}
