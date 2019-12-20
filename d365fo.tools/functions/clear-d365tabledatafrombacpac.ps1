3538e244315f495dbfcd894ca516d2ea828e5465
Mötz Jensen <motz@e-s.dk>
2019-12-20 09:08:15 +0100

    Docs: Formatted CBH

--- d365fo.tools/functions/clear-d365tabledatafrombacpac.ps1
+++ d365fo.tools/functions/clear-d365tabledatafrombacpac.ps1
@@ -1,82 +1,83 @@
-﻿<#
-.SYNOPSIS
-Clear out data for a table inside the bacpac file
-
-.DESCRIPTION
-Remove all data for a table inside a bacpac file, before restoring it into your SQL Server / Azure SQL DB
-
-It will extract the bacpac file as a zip archive, locate the desired table and remove the data that otherwise would have been loaded
-
-It will re-zip / compress a new bacpac file for you
-
-.PARAMETER Path
-Path to the bacpac file that you want to work against
-
-It can also be a zip file
-
-.PARAMETER TableName
-Name of the table that you want to delete the data for
-
-Supports an array of table names
-
-If a schema name isn't supplied as part of the table name, the cmdlet will prefix it with "dbo."
-
-.PARAMETER OutputPath
-Path to where you want the updated bacpac file to be saved
-
-.PARAMETER ExtractionPath
-Path to where you want the cmdlet to extract the files from the bacpac file while it deletes data
-
-The default value is "c:\temp\d365fo.tools\BacpacExtractions"
-
-When working the cmdlet will create a sub-folder named like the bacpac file
-
-.PARAMETER KeepFiles
-Switch to instruct the cmdlet to keep the extracted files and folders
-
-This will leave the files in place, after the deletion of the desired data
-
-.EXAMPLE
-PS C:\> Clear-D365TableDataFromBacpac -Path "C:\Temp\AxDB.bacpac" -TableName "BATCHJOBHISTORY" -OutputPath "C:\Temp\AXBD_Cleaned.bacpac"
-
-This will remove the data from the BatchJobHistory table from inside the bacpac file.
-
-It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
-It uses "BATCHJOBHISTORY" as the TableName to delete data from.
-It uses "C:\Temp\AXBD_Cleaned.bacpac" as the OutputPath to where it will store the updated bacpac file.
-It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
-
-It will delete the extracted files after storing the updated bacpac file.
-
-.EXAMPLE
-PS C:\> Clear-D365TableDataFromBacpac -Path "C:\Temp\AxDB.bacpac" -TableName "BATCHJOBHISTORY" -OutputPath "C:\Temp\AXBD_Cleaned.bacpac" -KeepFiles
-
-This will remove the data from the BatchJobHistory table from inside the bacpac file.
-
-It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
-It uses "BATCHJOBHISTORY" as the TableName to delete data from.
-It uses "C:\Temp\AXBD_Cleaned.bacpac" as the OutputPath to where it will store the updated bacpac file.
-It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
-
-It will NOT delete the extracted files after storing the updated bacpac file.
-
-.EXAMPLE
-PS C:\> Clear-D365TableDataFromBacpac -Path "C:\Temp\AxDB.bacpac" -TableName "dbo.BATCHHISTORY","BATCHJOBHISTORY" -OutputPath "C:\Temp\AXBD_Cleaned.bacpac"
-
-This will remove the data from the BatchJobHistory table from inside the bacpac file.
-
-It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
-It uses "dbo.BATCHHISTORY","BATCHJOBHISTORY" as the TableName to delete data from.
-It uses "C:\Temp\AXBD_Cleaned.bacpac" as the OutputPath to where it will store the updated bacpac file.
-It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
-
-It will delete the extracted files after storing the updated bacpac file.
-
-.NOTES
-Tags: Bacpac, Servicing, Data, Deletion, SqlPackage
-
-Author: Mötz Jensen (@Splaxi)
-
+﻿
+<#
+    .SYNOPSIS
+        Clear out data for a table inside the bacpac file
+        
+    .DESCRIPTION
+        Remove all data for a table inside a bacpac file, before restoring it into your SQL Server / Azure SQL DB
+        
+        It will extract the bacpac file as a zip archive, locate the desired table and remove the data that otherwise would have been loaded
+        
+        It will re-zip / compress a new bacpac file for you
+        
+    .PARAMETER Path
+        Path to the bacpac file that you want to work against
+        
+        It can also be a zip file
+        
+    .PARAMETER TableName
+        Name of the table that you want to delete the data for
+        
+        Supports an array of table names
+        
+        If a schema name isn't supplied as part of the table name, the cmdlet will prefix it with "dbo."
+        
+    .PARAMETER OutputPath
+        Path to where you want the updated bacpac file to be saved
+        
+    .PARAMETER ExtractionPath
+        Path to where you want the cmdlet to extract the files from the bacpac file while it deletes data
+        
+        The default value is "c:\temp\d365fo.tools\BacpacExtractions"
+        
+        When working the cmdlet will create a sub-folder named like the bacpac file
+        
+    .PARAMETER KeepFiles
+        Switch to instruct the cmdlet to keep the extracted files and folders
+        
+        This will leave the files in place, after the deletion of the desired data
+        
+    .EXAMPLE
+        PS C:\> Clear-D365TableDataFromBacpac -Path "C:\Temp\AxDB.bacpac" -TableName "BATCHJOBHISTORY" -OutputPath "C:\Temp\AXBD_Cleaned.bacpac"
+        
+        This will remove the data from the BatchJobHistory table from inside the bacpac file.
+        
+        It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
+        It uses "BATCHJOBHISTORY" as the TableName to delete data from.
+        It uses "C:\Temp\AXBD_Cleaned.bacpac" as the OutputPath to where it will store the updated bacpac file.
+        It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
+        
+        It will delete the extracted files after storing the updated bacpac file.
+        
+    .EXAMPLE
+        PS C:\> Clear-D365TableDataFromBacpac -Path "C:\Temp\AxDB.bacpac" -TableName "BATCHJOBHISTORY" -OutputPath "C:\Temp\AXBD_Cleaned.bacpac" -KeepFiles
+        
+        This will remove the data from the BatchJobHistory table from inside the bacpac file.
+        
+        It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
+        It uses "BATCHJOBHISTORY" as the TableName to delete data from.
+        It uses "C:\Temp\AXBD_Cleaned.bacpac" as the OutputPath to where it will store the updated bacpac file.
+        It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
+        
+        It will NOT delete the extracted files after storing the updated bacpac file.
+        
+    .EXAMPLE
+        PS C:\> Clear-D365TableDataFromBacpac -Path "C:\Temp\AxDB.bacpac" -TableName "dbo.BATCHHISTORY","BATCHJOBHISTORY" -OutputPath "C:\Temp\AXBD_Cleaned.bacpac"
+        
+        This will remove the data from the BatchJobHistory table from inside the bacpac file.
+        
+        It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
+        It uses "dbo.BATCHHISTORY","BATCHJOBHISTORY" as the TableName to delete data from.
+        It uses "C:\Temp\AXBD_Cleaned.bacpac" as the OutputPath to where it will store the updated bacpac file.
+        It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
+        
+        It will delete the extracted files after storing the updated bacpac file.
+        
+    .NOTES
+        Tags: Bacpac, Servicing, Data, Deletion, SqlPackage
+        
+        Author: Mötz Jensen (@Splaxi)
+        
 #>
 
 function Clear-D365TableDataFromBacpac {
--- d365fo.tools/functions/export-d365modelfilefrombacpac.ps1
+++ d365fo.tools/functions/export-d365modelfilefrombacpac.ps1
@@ -1,75 +1,76 @@
-﻿<#
-.SYNOPSIS
-Extract the "model.xml" from the bacpac file
-
-.DESCRIPTION
-Extract the "model.xml" file from inside the bacpac file
-
-This can be used to update SQL Server options for how the SqlPackage.exe should import the bacpac file into your SQL Server / Azure SQL DB
-
-.PARAMETER Path
-Path to the bacpac file that you want to work against
-
-It can also be a zip file
-
-.PARAMETER OutputPath
-Path to where you want the updated bacpac file to be saved
-
-.PARAMETER ExtractionPath
-Path to where you want the cmdlet to extract the files from the bacpac file while it deletes data
-
-The default value is "c:\temp\d365fo.tools\BacpacExtractions"
-
-When working the cmdlet will create a sub-folder named like the bacpac file
-
-.PARAMETER Force
-Switch to instruct the cmdlet to overwrite the "model.xml" specified in the OutputPath
-
-.PARAMETER KeepFiles
-Switch to instruct the cmdlet to keep the extracted files and folders
-
-This will leave the files in place, after the extraction of the "model.xml" file
-
-.EXAMPLE
-PS C:\> Export-d365ModelFileFromBacpac -Path "C:\Temp\AxDB.bacpac" -OutputPath "C:\Temp\model.xml"
-
-This will extract the "model.xml" file from inside the bacpac file.
-
-It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
-It uses "C:\Temp\model.xml" as the OutputPath to where it will store the extracted "model.xml" file.
-It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
-
-It will delete the extracted files after extracting the "model.xml" file.
-
-.EXAMPLE
-PS C:\> Export-d365ModelFileFromBacpac -Path "C:\Temp\AxDB.bacpac" -OutputPath "C:\Temp\model.xml" -Force
-
-This will extract the "model.xml" file from inside the bacpac file.
-
-It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
-It uses "C:\Temp\model.xml" as the OutputPath to where it will store the extracted "model.xml" file.
-It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
-
-It will override the "C:\Temp\model.xml" if already present.
-
-It will delete the extracted files after extracting the "model.xml" file.
-
-.EXAMPLE
-PS C:\> Export-d365ModelFileFromBacpac -Path "C:\Temp\AxDB.bacpac" -OutputPath "C:\Temp\model.xml" -KeepFiles
-
-This will extract the "model.xml" file from inside the bacpac file.
-
-It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
-It uses "C:\Temp\model.xml" as the OutputPath to where it will store the extracted "model.xml" file.
-It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
-
-It will NOT delete the extracted files after extracting the "model.xml" file.
-
-.NOTES
-Tags: Bacpac, Servicing, Data, SqlPackage, Sql Server Options, Collation
-
-Author: Mötz Jensen (@Splaxi)
-
+﻿
+<#
+    .SYNOPSIS
+        Extract the "model.xml" from the bacpac file
+        
+    .DESCRIPTION
+        Extract the "model.xml" file from inside the bacpac file
+        
+        This can be used to update SQL Server options for how the SqlPackage.exe should import the bacpac file into your SQL Server / Azure SQL DB
+        
+    .PARAMETER Path
+        Path to the bacpac file that you want to work against
+        
+        It can also be a zip file
+        
+    .PARAMETER OutputPath
+        Path to where you want the updated bacpac file to be saved
+        
+    .PARAMETER ExtractionPath
+        Path to where you want the cmdlet to extract the files from the bacpac file while it deletes data
+        
+        The default value is "c:\temp\d365fo.tools\BacpacExtractions"
+        
+        When working the cmdlet will create a sub-folder named like the bacpac file
+        
+    .PARAMETER Force
+        Switch to instruct the cmdlet to overwrite the "model.xml" specified in the OutputPath
+        
+    .PARAMETER KeepFiles
+        Switch to instruct the cmdlet to keep the extracted files and folders
+        
+        This will leave the files in place, after the extraction of the "model.xml" file
+        
+    .EXAMPLE
+        PS C:\> Export-d365ModelFileFromBacpac -Path "C:\Temp\AxDB.bacpac" -OutputPath "C:\Temp\model.xml"
+        
+        This will extract the "model.xml" file from inside the bacpac file.
+        
+        It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
+        It uses "C:\Temp\model.xml" as the OutputPath to where it will store the extracted "model.xml" file.
+        It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
+        
+        It will delete the extracted files after extracting the "model.xml" file.
+        
+    .EXAMPLE
+        PS C:\> Export-d365ModelFileFromBacpac -Path "C:\Temp\AxDB.bacpac" -OutputPath "C:\Temp\model.xml" -Force
+        
+        This will extract the "model.xml" file from inside the bacpac file.
+        
+        It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
+        It uses "C:\Temp\model.xml" as the OutputPath to where it will store the extracted "model.xml" file.
+        It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
+        
+        It will override the "C:\Temp\model.xml" if already present.
+        
+        It will delete the extracted files after extracting the "model.xml" file.
+        
+    .EXAMPLE
+        PS C:\> Export-d365ModelFileFromBacpac -Path "C:\Temp\AxDB.bacpac" -OutputPath "C:\Temp\model.xml" -KeepFiles
+        
+        This will extract the "model.xml" file from inside the bacpac file.
+        
+        It uses "C:\Temp\AxDB.bacpac" as the Path for the bacpac file.
+        It uses "C:\Temp\model.xml" as the OutputPath to where it will store the extracted "model.xml" file.
+        It uses the default ExtractionPath folder "C:\Temp\d365fo.tools\BacpacExtractions".
+        
+        It will NOT delete the extracted files after extracting the "model.xml" file.
+        
+    .NOTES
+        Tags: Bacpac, Servicing, Data, SqlPackage, Sql Server Options, Collation
+        
+        Author: Mötz Jensen (@Splaxi)
+        
 #>
 
 function Export-D365ModelFileFromBacpac {
