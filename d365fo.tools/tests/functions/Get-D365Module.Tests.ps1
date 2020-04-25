111c378a15d8cd478bfcad0fd365578df40e444e
Mötz Jensen <motz@e-s.dk>
2020-04-25 23:10:38 +0200

    Tests: Parameter unit test updated

--- d365fo.tools/tests/functions/Get-D365Module.Tests.ps1
+++ d365fo.tools/tests/functions/Get-D365Module.Tests.ps1
@@ -24,6 +24,19 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter ExcludeBinaryModules' {
+			$parameter = (Get-Command Get-D365Module).Parameters['ExcludeBinaryModules']
+			$parameter.Name | Should -Be 'ExcludeBinaryModules'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 		It 'Should have the expected parameter BinDir' {
 			$parameter = (Get-Command Get-D365Module).Parameters['BinDir']
 			$parameter.Name | Should -Be 'BinDir'
@@ -55,7 +68,7 @@
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -
-		__AllParameterSets -Name -BinDir -PackageDirectory
+		__AllParameterSets -Name -ExcludeBinaryModules -BinDir -PackageDirectory
 		#>
 	}
 
