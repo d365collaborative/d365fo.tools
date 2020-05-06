c67537da7551a5dc985778f60d77459635e1b27e
Mötz Jensen <motz@e-s.dk>
2020-05-06 14:05:42 +0200

    Use older version of PSScriptAnalyzer for now

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,7 +1,9 @@
 ﻿Write-Host "Working on the machine named: $($env:computername)"
 Write-Host "The user running is: $($env:UserName)"
 
-$modules = @("Pester", "PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement","ImportExcel")
+$modules = @("Pester", "PSFramework", "Azure.Storage", "AzureAd", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement","ImportExcel")
+
+Install-Module -Name "PSScriptAnalyzer" -Force -SkipPublisherCheck -AllowClobber -RequiredVersion 1.18.3
 
 foreach ($module in $modules) {
     Write-Host "Installing $module" -ForegroundColor Cyan
