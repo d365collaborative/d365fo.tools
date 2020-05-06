206d8e5b6f16e024c246175596516d8f647f297d
Mötz Jensen <motz@e-s.dk>
2020-05-06 14:45:49 +0200

    Tests: unit test updated.

--- d365fo.tools/tests/functions/Get-D365TfsWorkspace.Tests.ps1
+++ d365fo.tools/tests/functions/Get-D365TfsWorkspace.Tests.ps1
@@ -8,7 +8,7 @@
 	
 	Describe "Ensuring unchanged command signature" {
 		It "should have the expected parameter sets" {
-			(Get-Command Get-D365TfsWorkspace).ParameterSets.Name | Should -Be 'Default'
+			(Get-Command Get-D365TfsWorkspace).ParameterSets.Name | Should -Be '__AllParameterSets'
 		}
 		
 		It 'Should have the expected parameter Path' {
@@ -16,33 +16,33 @@
 			$parameter.Name | Should -Be 'Path'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
-			$parameter.ParameterSets.Keys | Should -Be 'Default'
-			$parameter.ParameterSets.Keys | Should -Contain 'Default'
-			$parameter.ParameterSets['Default'].IsMandatory | Should -Be $False
-			$parameter.ParameterSets['Default'].Position | Should -Be 1
-			$parameter.ParameterSets['Default'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['Default'].ValueFromPipelineByPropertyName | Should -Be $False
-			$parameter.ParameterSets['Default'].ValueFromRemainingArguments | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 0
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
 		It 'Should have the expected parameter TfsUri' {
 			$parameter = (Get-Command Get-D365TfsWorkspace).Parameters['TfsUri']
 			$parameter.Name | Should -Be 'TfsUri'
 			$parameter.ParameterType.ToString() | Should -Be System.String
 			$parameter.IsDynamic | Should -Be $False
-			$parameter.ParameterSets.Keys | Should -Be 'Default'
-			$parameter.ParameterSets.Keys | Should -Contain 'Default'
-			$parameter.ParameterSets['Default'].IsMandatory | Should -Be $False
-			$parameter.ParameterSets['Default'].Position | Should -Be 2
-			$parameter.ParameterSets['Default'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['Default'].ValueFromPipelineByPropertyName | Should -Be $True
-			$parameter.ParameterSets['Default'].ValueFromRemainingArguments | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 1
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $True
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
 	}
 	
-	Describe "Testing parameterset Default" {
+	Describe "Testing parameterset __AllParameterSets" {
 		<#
-		Default -
-		Default -Path -TfsUri
+		__AllParameterSets -
+		__AllParameterSets -Path -TfsUri
 		#>
 	}
 
