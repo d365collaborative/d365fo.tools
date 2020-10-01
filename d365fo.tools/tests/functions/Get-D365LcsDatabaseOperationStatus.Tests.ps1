43b1fce22667822dbe92b0defd79328ecacbebb3
Mötz Jensen <motz@e-s.dk>
2020-10-01 20:06:21 +0200

    Tests: Parameter unit tests updated to reflect refactor

--- d365fo.tools/tests/functions/Get-D365LcsDatabaseOperationStatus.Tests.ps1
+++ d365fo.tools/tests/functions/Get-D365LcsDatabaseOperationStatus.Tests.ps1
@@ -102,12 +102,25 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter EnableException' {
+			$parameter = (Get-Command Get-D365LcsDatabaseOperationStatus).Parameters['EnableException']
+			$parameter.Name | Should -Be 'EnableException'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 	}
 	
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -OperationActivityId -EnvironmentId
-		__AllParameterSets -ProjectId -BearerToken -OperationActivityId -EnvironmentId -LcsApiUri -WaitForCompletion -SleepInSeconds
+		__AllParameterSets -ProjectId -BearerToken -OperationActivityId -EnvironmentId -LcsApiUri -WaitForCompletion -SleepInSeconds -EnableException
 		#>
 	}
 
