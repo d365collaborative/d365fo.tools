c41e580fea9125d6105dfc7142b4c66943987648
Mötz Jensen <motz@e-s.dk>
2021-02-02 12:54:29 +0100

    Feature: Add kill option for restart-d365environment and stop-d365environment.

--- d365fo.tools/functions/stop-d365environment.ps1
+++ d365fo.tools/functions/stop-d365environment.ps1
@@ -29,6 +29,9 @@
     .PARAMETER DMF
         Start the Data Management Framework service
         
+    .PARAMETER Kill
+        Instructs the cmdlet to kill the service(s) that you want to stop
+
     .PARAMETER ShowOriginalProgress
         Instruct the cmdlet to show the standard output in the console
         
@@ -64,6 +67,12 @@
         
         This will stop the FinancialReporter and DMF services on the machine.
         
+    .EXAMPLE
+        PS C:\> Stop-D365Environment -All -Kill
+        
+        This will stop all D365FO services on the machine.
+        It will use the Kill parameter to make sure that the services is stopped.
+
     .NOTES
         Author: Mötz Jensen (@Splaxi)
         
@@ -95,6 +104,10 @@ function Stop-D365Environment {
 
         [Parameter(Mandatory = $false, ParameterSetName = 'Specific', Position = 6 )]
         [Parameter(Mandatory = $false, ParameterSetName = 'Default', Position = 6 )]
+        [switch] $Kill,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'Specific', Position = 7 )]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default', Position = 7 )]
         [switch] $ShowOriginalProgress
     )
 
@@ -109,22 +122,32 @@ function Stop-D365Environment {
     }
 
     $warningActionValue = "SilentlyContinue"
-    if ($ShowOriginalProgress) {$warningActionValue = "Continue"}
+    if ($ShowOriginalProgress) { $warningActionValue = "Continue" }
 
     $Params = Get-DeepClone $PSBoundParameters
-    if ($Params.ContainsKey("ComputerName")) {$null = $Params.Remove("ComputerName")}
-    if ($Params.ContainsKey("ShowOriginalProgress")) {$null = $Params.Remove("ShowOriginalProgress")}
+    if ($Params.ContainsKey("ComputerName")) { $null = $Params.Remove("ComputerName") }
+    if ($Params.ContainsKey("ShowOriginalProgress")) { $null = $Params.Remove("ShowOriginalProgress") }
 
     $Services = Get-ServiceList @Params
     
     $Results = foreach ($server in $ComputerName) {
         Write-PSFMessage -Level Verbose -Message "Working against: $server - stopping services"
-        Get-Service -ComputerName $server -Name $Services -ErrorAction SilentlyContinue | Stop-Service -Force -ErrorAction SilentlyContinue -WarningAction $warningActionValue
+
+        if (-not $Kill) {
+            Get-Service -ComputerName $server -Name $Services -ErrorAction SilentlyContinue | Stop-Service -Force -ErrorAction SilentlyContinue -WarningAction $warningActionValue
+        }
+        else {
+            Get-Service -ComputerName $server -Name $Services -ErrorAction SilentlyContinue | ForEach-Object {
+                $service = Get-CimInstance -ClassName "win32_service" -Filter "Name = '$($_.Name)'"
+
+                Stop-Process -Id "$($service.ProcessId)" -Force -ErrorAction SilentlyContinue -WarningAction $warningActionValue
+            }
+        }
     }
 
     $Results = foreach ($server in $ComputerName) {
         Write-PSFMessage -Level Verbose -Message "Working against: $server - listing services"
-        Get-Service -ComputerName $server -Name $Services -ErrorAction SilentlyContinue | Select-Object @{Name = "Server"; Expression = {$Server}}, Name, Status, StartType, DisplayName
+        Get-Service -ComputerName $server -Name $Services -ErrorAction SilentlyContinue | Select-Object @{Name = "Server"; Expression = { $Server } }, Name, Status, StartType, DisplayName
     }
     
     Write-PSFMessage -Level Verbose "Results are: $Results" -Target ($Results.Name -join ",")
