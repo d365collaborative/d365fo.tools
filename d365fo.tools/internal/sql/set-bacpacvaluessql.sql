e16a0021a3b4068994b725e074ecb856d2f04b1e
Mötz Jensen <motz@e-s.dk>
2021-02-18 12:06:05 +0100

    Fix: Implement better logic to handle missing logins

--- d365fo.tools/internal/sql/set-bacpacvaluessql.sql
+++ d365fo.tools/internal/sql/set-bacpacvaluessql.sql
@@ -9,37 +9,56 @@ DROP USER IF EXISTS [axdbadmin]
 DROP USER IF EXISTS [axdeployextuser]
 DROP USER IF EXISTS [NT AUTHORITY\NETWORK SERVICE]
 
-CREATE USER axdeployuser FROM LOGIN axdeployuser
-EXEC sp_addrolemember 'db_owner', 'axdeployuser'
-
-CREATE USER axdbadmin FROM LOGIN axdbadmin
-EXEC sp_addrolemember 'db_owner', 'axdbadmin'
-
-CREATE USER axmrruntimeuser FROM LOGIN axmrruntimeuser
-EXEC sp_addrolemember 'db_datareader', 'axmrruntimeuser'
-EXEC sp_addrolemember 'db_datawriter', 'axmrruntimeuser'
-
-CREATE USER axretaildatasyncuser FROM LOGIN axretaildatasyncuser
-IF (DATABASE_PRINCIPAL_ID('DataSyncUsersRole') IS NOT NULL)
+IF EXISTS (SELECT * FROM sys.syslogins WHERE NAME = 'axdeployuser')
 BEGIN
-    EXEC sp_addrolemember 'DataSyncUsersRole', 'axretaildatasyncuser'
+	CREATE USER axdeployuser FROM LOGIN axdeployuser
+	EXEC sp_addrolemember 'db_owner', 'axdeployuser'
 END
 
-CREATE USER axretailruntimeuser FROM LOGIN axretailruntimeuser
-IF (DATABASE_PRINCIPAL_ID('UsersRole') IS NOT NULL)
+IF EXISTS (SELECT * FROM sys.syslogins WHERE NAME = 'axdbadmin')
 BEGIN
-    EXEC sp_addrolemember 'UsersRole', 'axretailruntimeuser'
-    
-END
-IF (DATABASE_PRINCIPAL_ID('ReportUsersRole') IS NOT NULL)
-BEGIN
-    EXEC sp_addrolemember 'ReportUsersRole', 'axretailruntimeuser'
+	CREATE USER axdbadmin FROM LOGIN axdbadmin
+	EXEC sp_addrolemember 'db_owner', 'axdbadmin'
 END
 
-CREATE USER axdeployextuser FROM LOGIN axdeployextuser
-IF (DATABASE_PRINCIPAL_ID('DeployExtensibilityRole') IS NOT NULL)
+IF EXISTS (SELECT * FROM sys.syslogins WHERE NAME = 'axmrruntimeuser')
 BEGIN
-    EXEC sp_addrolemember 'DeployExtensibilityRole', 'axdeployextuser'
+	CREATE USER axmrruntimeuser FROM LOGIN axmrruntimeuser
+	EXEC sp_addrolemember 'db_datareader', 'axmrruntimeuser'
+	EXEC sp_addrolemember 'db_datawriter', 'axmrruntimeuser'
+END
+
+IF EXISTS (SELECT * FROM sys.syslogins WHERE NAME = 'axretaildatasyncuser')
+BEGIN
+	CREATE USER axretaildatasyncuser FROM LOGIN axretaildatasyncuser
+	IF (DATABASE_PRINCIPAL_ID('DataSyncUsersRole') IS NOT NULL)
+	BEGIN
+		EXEC sp_addrolemember 'DataSyncUsersRole', 'axretaildatasyncuser'
+	END
+END
+
+IF EXISTS (SELECT * FROM sys.syslogins WHERE NAME = 'axretailruntimeuser')
+BEGIN
+	CREATE USER axretailruntimeuser FROM LOGIN axretailruntimeuser
+	IF (DATABASE_PRINCIPAL_ID('UsersRole') IS NOT NULL)
+	BEGIN
+		EXEC sp_addrolemember 'UsersRole', 'axretailruntimeuser'
+
+	END
+	
+	IF (DATABASE_PRINCIPAL_ID('ReportUsersRole') IS NOT NULL)
+	BEGIN
+		EXEC sp_addrolemember 'ReportUsersRole', 'axretailruntimeuser'
+	END
+END
+
+IF EXISTS (SELECT * FROM sys.syslogins WHERE NAME = 'axdeployextuser')
+BEGIN
+	CREATE USER axdeployextuser FROM LOGIN axdeployextuser
+	IF (DATABASE_PRINCIPAL_ID('DeployExtensibilityRole') IS NOT NULL)
+	BEGIN
+		EXEC sp_addrolemember 'DeployExtensibilityRole', 'axdeployextuser'
+	END
 END
 
 CREATE USER [NT AUTHORITY\NETWORK SERVICE] FROM LOGIN [NT AUTHORITY\NETWORK SERVICE]
@@ -54,9 +73,11 @@ FROM docuvalue T1
 WHERE T1.storageproviderid = 1 --Azure storage
 
 
-IF((SELECT 1 FROM SYS.CHANGE_TRACKING_DATABASES WHERE DATABASE_ID = DB_ID('@DATABASENAME')) IS NULL)
+IF((SELECT 1
+FROM SYS.CHANGE_TRACKING_DATABASES
+WHERE DATABASE_ID = DB_ID('@DATABASENAME')) IS NULL)
 BEGIN
-    ALTER DATABASE [@DATABASENAME] SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 6 DAYS, AUTO_CLEANUP = ON)
+	ALTER DATABASE [@DATABASENAME] SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 6 DAYS, AUTO_CLEANUP = ON)
 END
 
 ;--GO
@@ -67,8 +88,12 @@ DROP PROCEDURE IF EXISTS SP_ConfigureTablesForChangeTracking_V2
 DECLARE @RFTXNAME NVARCHAR(MAX);
 DECLARE @RFTXSQL NVARCHAR(MAX);
 DECLARE retail_ftx CURSOR FOR
-SELECT OBJECT_SCHEMA_NAME(object_id) + '.' + OBJECT_NAME(object_id) fullname FROM SYS.FULLTEXT_INDEXES
-	WHERE FULLTEXT_CATALOG_ID = (SELECT TOP 1 FULLTEXT_CATALOG_ID FROM SYS.FULLTEXT_CATALOGS WHERE NAME = 'COMMERCEFULLTEXTCATALOG');
+SELECT OBJECT_SCHEMA_NAME(object_id) + '.' + OBJECT_NAME(object_id) fullname
+FROM SYS.FULLTEXT_INDEXES
+WHERE FULLTEXT_CATALOG_ID = (SELECT TOP 1
+	FULLTEXT_CATALOG_ID
+FROM SYS.FULLTEXT_CATALOGS
+WHERE NAME = 'COMMERCEFULLTEXTCATALOG');
 OPEN retail_ftx;
 FETCH NEXT FROM retail_ftx INTO @RFTXNAME;
 
@@ -86,6 +111,6 @@ BEGIN CATCH
 	PRINT error_message()
 END CATCH
 
-CLOSE retail_ftx;  
-DEALLOCATE retail_ftx; 
+CLOSE retail_ftx;
+DEALLOCATE retail_ftx;
 -- End Refresh Retail FullText Catalogs
