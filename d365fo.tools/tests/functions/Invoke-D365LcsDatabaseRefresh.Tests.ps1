0a39deb1ec8824b01bcb387704e807595044c269
Mötz Jensen <motz@e-s.dk>
2021-05-02 22:31:57 +0200

    Test: Updated parameter tests to reflect refactored code

--- d365fo.tools/tests/functions/Invoke-D365LcsDatabaseRefresh.Tests.ps1
+++ d365fo.tools/tests/functions/Invoke-D365LcsDatabaseRefresh.Tests.ps1
@@ -89,6 +89,19 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter FailOnErrorMessage' {
+			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['FailOnErrorMessage']
+			$parameter.Name | Should -Be 'FailOnErrorMessage'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 		It 'Should have the expected parameter EnableException' {
 			$parameter = (Get-Command Invoke-D365LcsDatabaseRefresh).Parameters['EnableException']
 			$parameter.Name | Should -Be 'EnableException'
@@ -107,7 +120,7 @@
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -SourceEnvironmentId -TargetEnvironmentId
-		__AllParameterSets -ProjectId -BearerToken -SourceEnvironmentId -TargetEnvironmentId -LcsApiUri -SkipInitialStatusFetch -EnableException
+		__AllParameterSets -ProjectId -BearerToken -SourceEnvironmentId -TargetEnvironmentId -LcsApiUri -SkipInitialStatusFetch -FailOnErrorMessage -EnableException
 		#>
 	}
 
--- d365fo.tools/tests/functions/Invoke-D365LcsEnvironmentStop.Tests.ps1
+++ d365fo.tools/tests/functions/Invoke-D365LcsEnvironmentStop.Tests.ps1
@@ -63,6 +63,19 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter FailOnErrorMessage' {
+			$parameter = (Get-Command Invoke-D365LcsEnvironmentStop).Parameters['FailOnErrorMessage']
+			$parameter.Name | Should -Be 'FailOnErrorMessage'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 		It 'Should have the expected parameter EnableException' {
 			$parameter = (Get-Command Invoke-D365LcsEnvironmentStop).Parameters['EnableException']
 			$parameter.Name | Should -Be 'EnableException'
@@ -81,7 +94,7 @@
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -EnvironmentId
-		__AllParameterSets -ProjectId -BearerToken -EnvironmentId -LcsApiUri -EnableException
+		__AllParameterSets -ProjectId -BearerToken -EnvironmentId -LcsApiUri -FailOnErrorMessage -EnableException
 		#>
 	}
 
