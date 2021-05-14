8154ef2769e43a1c5a26371d52296732e8ef965b
Mötz Jensen <motz@e-s.dk>
2021-05-14 23:02:08 +0200

    Fix: Add missing modules that are a prerequisites.

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -2,7 +2,7 @@
 Write-Host "The user running is: $($env:UserName)"
 
 # $modules = @("PSFramework", "Azure.Storage", "AzureAd", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement","ImportExcel","PSScriptAnalyzer")
-$modules = @("PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification")
+$modules = @("PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification", "PSOAuthHelper", "ImportExcel")
 
 Install-Module "Pester" -MaximumVersion 4.99.99 -Force -SkipPublisherCheck -AllowClobber
 
