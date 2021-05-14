c38bf716223b51c85b98f352a73cd91a897abf3a
Mötz Jensen <motz@e-s.dk>
2021-05-14 22:52:27 +0200

    Refactor: Optimize unit test performance.

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,15 +1,19 @@
 ﻿Write-Host "Working on the machine named: $($env:computername)"
 Write-Host "The user running is: $($env:UserName)"
 
-$modules = @("PSFramework", "Azure.Storage", "AzureAd", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement","ImportExcel","PSScriptAnalyzer")
+# $modules = @("PSFramework", "Azure.Storage", "AzureAd", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement","ImportExcel","PSScriptAnalyzer")
+$modules = @("PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification")
 
 Install-Module "Pester" -MaximumVersion 4.99.99 -Force -SkipPublisherCheck -AllowClobber
-#Install-Module -Name "PSScriptAnalyzer" -Force -SkipPublisherCheck -AllowClobber #-RequiredVersion 1.18.3
 
 foreach ($module in $modules) {
-    Write-Host "Installing $module" -ForegroundColor Cyan
-    Install-Module $module -Force -SkipPublisherCheck -AllowClobber
-    Import-Module $module -Force -PassThru
-}
+    
+    $module = Get-InstalledModule -Name $module -ErrorAction SilentlyContinue
 
-#(Get-Module -ListAvailable).ModuleBase
+    if ($null -eq $module) {
+        Write-Host "Installing $module" -ForegroundColor Cyan
+        Install-Module -Name $module -Force -Confirm:$false -Scope CurrentUser -AllowClobber -SkipPublisherCheck
+    }
+
+    Import-Module $module -Force
+}
--- build/vsts-validate.ps1
+++ build/vsts-validate.ps1
@@ -1,4 +1,12 @@
-﻿# Guide for available variables and working with secrets:
+﻿param (
+    $TestGeneral = $true,
+	
+    $TestFunctions = $true,
+
+    $TestScriptAnalyzer = $true
+)
+
+# Guide for available variables and working with secrets:
 # https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
 
 # Needs to ensure things are Done Right and only legal commits to master get built
@@ -8,16 +16,17 @@
 Write-Host "Working on the machine named: $($env:computername)"
 Write-Host "The user running is: $($env:UserName)"
 
-# $modules = @("Pester", "PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification")
 $modules = @("PSFramework", "PSScriptAnalyzer", "Azure.Storage", "AzureAd", "PSNotification")
 
 foreach ($module in $modules) {
-    Write-Host "Importing $module" -ForegroundColor Cyan
-    Import-Module $module -Force -PassThru
+    $module = Get-Module -Name $module -ErrorAction SilentlyContinue
+
+    if ($null -eq $module) {
+        Write-Host "Importing $module" -ForegroundColor Cyan
+        Import-Module $module -Force
+    }
 }
 
 Import-Module "Pester" -MaximumVersion 4.99.99 -Force
 
-#(Get-Module -ListAvailable).ModuleBase
-
-& "$PSScriptRoot\..\d365fo.tools\tests\pester.ps1"
+& "$PSScriptRoot\..\d365fo.tools\tests\pester.ps1" -TestGeneral $TestGeneral -TestFunctions $TestFunctions -TestScriptAnalyzer $TestScriptAnalyzer
