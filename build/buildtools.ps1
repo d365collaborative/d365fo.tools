78f19f85677ec9a37e79038db163305dfea37bef
Florian Hopfner <florian.hopfner@inway.de>
2023-06-11 15:21:27 +0000

    :construction_worker: add action to format comment based help

---
+++ build/buildtools.ps1
@@ -0,0 +1,19 @@
+# Installs the modules required for the automatic text generation.
+# See also https://github.com/d365collaborative/d365fo.tools/wiki/Building-tools
+
+Write-Host "Working on the machine named: $($env:computername)"
+Write-Host "The user running is: $($env:UserName)"
+
+$modules = @("PSModuleDevelopment", "platyPS")
+
+foreach ($item in $modules) {
+    
+    $module = Get-InstalledModule -Name $item -ErrorAction SilentlyContinue
+
+    if ($null -eq $module) {
+        Write-Host "Installing $item" -ForegroundColor Cyan
+        Install-Module -Name $item -Force -Confirm:$false -Scope CurrentUser -AllowClobber -SkipPublisherCheck
+    }
+
+    Import-Module $item -Force
+}
