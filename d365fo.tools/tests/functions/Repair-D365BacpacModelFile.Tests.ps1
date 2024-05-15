3686f2a04bbb08fdcff7a6acee9ea819965797cd
Mötz Jensen <motz@e-s.dk>
2024-05-15 11:01:14 +0200

    Fix: Add missing unit test for the new paramter

--- d365fo.tools/tests/functions/Repair-D365BacpacModelFile.Tests.ps1
+++ d365fo.tools/tests/functions/Repair-D365BacpacModelFile.Tests.ps1
@@ -89,12 +89,25 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter Force' {
+			$parameter = (Get-Command Repair-D365BacpacModelFile).Parameters['Force']
+			$parameter.Name | Should -Be 'Force'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 	}
 	
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -Path
-		__AllParameterSets -Path -OutputPath -PathRepairSimple -PathRepairQualifier -PathRepairReplace -KeepFiles
+		__AllParameterSets -Path -OutputPath -PathRepairSimple -PathRepairQualifier -PathRepairReplace -KeepFiles -Force
 		#>
 	}
 
