0c390f2030431bf3233ef31a38b475f4e156881c
Florian Hopfner <florian.hopfner@inway.de>
2024-06-30 15:19:44 +0200

    add importing from Entra ID groups to Import-D365EntraIdUser

--- d365fo.tools/functions/import-d365entraiduser.ps1
+++ d365fo.tools/functions/import-d365entraiduser.ps1
@@ -4,6 +4,12 @@ function Import-D365EntraIdUser {
         [Parameter(Mandatory = $true, Position = 1, ParameterSetName = "UserListImport")]
         [string[]] $Users,
 
+        [Parameter(Mandatory = $true, Position = 1, ParameterSetName = "GroupNameImport")]
+        [String] $EntraIDGroupName,
+
+        [Parameter(Mandatory = $true, Position = 1, ParameterSetName = "GroupIdImport")]
+        [string] $EntraIDGroupId,
+
         [Parameter(Mandatory = $false, Position = 2)]
         [string] $StartupCompany = 'DAT',
 
@@ -26,13 +32,19 @@ function Import-D365EntraIdUser {
         [string] $ClientId,
 
         [Parameter(Mandatory = $false, Position = 9)]
-        [string] $CertificateName
+        [string] $CertificateName,
+
+        [Parameter(Mandatory = $false, Position = 12, ParameterSetName = "UserListImport")]
+        [switch] $SkipEntraID,
+
+        [Parameter(Mandatory = $false, Position = 13, ParameterSetName = "GroupNameImport")]
+        [switch] $ForceExactGroupName
 
       )
     
       begin {
         # TODO refactor into internal function
-        $microsoftGraphModules = @("Microsoft.Graph.Authentication", "Microsoft.Graph.Users")
+        $microsoftGraphModules = @("Microsoft.Graph.Authentication", "Microsoft.Graph.Users", "Microsoft.Graph.Groups")
         $microsoftGraphModules | ForEach-Object {
           if (-not (Get-Module -Name $_ -ListAvailable)) {
             Write-PSFMessage -Level Host -Message "Module $_ is not installed. Installing it now."
@@ -50,7 +62,9 @@ function Import-D365EntraIdUser {
         $instanceProvider = Get-InstanceIdentityProvider
         $canonicalProvider = Get-CanonicalIdentityProvider
 
-        Connect-MicrosoftGraph -TenantId $TenantId -ClientId $ClientId -CertificateName $CertificateName
+        if ($SkipEntraID -eq $false) {
+            Connect-MicrosoftGraph -TenantId $TenantId -ClientId $ClientId -CertificateName $CertificateName
+        }
         if (Test-PSFFunctionInterrupt) { return }
       }
 
@@ -58,6 +72,12 @@ function Import-D365EntraIdUser {
         switch ($PSCmdlet.ParameterSetName) {
           "UserListImport" {
             Import-UserList -Users $Users -Verbose
+          }
+          "GroupNameImport" {
+              Import-GroupMembers -GroupName $EntraIDGroupName -Verbose
+          }
+          "GroupIdImport" {
+              Import-GroupMembers -GroupId $EntraIDGroupId -Verbose
           }
         }
       }
@@ -165,68 +185,172 @@ function Import-UserList {
         [string[]] $Users
     )
 
-    $sqlCommand.Connection.Open()
+    $UsersToImport = New-Object -TypeName "System.Collections.ArrayList"
 
-    $users | ForEach-Object {
-        $entraIdUser = Get-MgUser -Filter "mail eq '$_'"
-        if ($null -eq $entraIdUser) {
-            Write-PSFMessage -Level Critical "Could not find user $_ in Entra ID."
-        }
-        $identityProvider = $canonicalProvider
-        Write-PSFMessage -Level Verbose -Message "Getting tenant from $($entraIdUser.Mail)."
-        $tenant = Get-TenantFromEmail $entraIdUser.Mail
+    foreach ($user in $Users) {
 
-        Write-PSFMessage -Level Verbose -Message "Getting domain from $($entraIdUser.Mail)."
-        $networkDomain = Get-NetworkDomain $entraIdUser.Mail
-
-        Write-PSFMessage -Level Verbose -Message "InstanceProvider : $InstanceProvider"
-        Write-PSFMessage -Level Verbose -Message "Tenant : $Tenant"
-
-        if ($entraIdUser.Mail.ToLower().Contains("outlook.com") -eq $true) {
-            $identityProvider = "live.com"
+        if ($SkipEntraID -eq $true) {
+            $name = Get-LoginFromEmail $user
+            $null = $UsersToImport.Add([PSCustomObject]@{
+                    Mail        = $user
+                    GivenName   = $name
+                    DisplayName = $name
+                    Id          = ''
+                })
         }
         else {
-            if ($instanceProvider.ToLower().Contains($tenant.ToLower()) -ne $True) {
-                Write-PSFMessage -Level Verbose -Message "Getting identity provider from  $($entraIdUser.Mail)."
-                $identityProvider = Get-IdentityProvider $entraIdUser.Mail
+            $entraIdUser = Get-MgUser -Filter "startswith(DisplayName, '$user') or startswith(UserPrincipalName, '$user')" # equivalent to `Get-AzureAdUser -SearchString $user`, see https://learn.microsoft.com/en-us/powershell/module/azuread/get-azureaduser?view=azureadps-2.0#example-3-search-among-retrieved-users
+            if ($null -eq $entraIdUser) {
+                Write-PSFMessage -Level Critical "Could not find user $_ in Entra ID."
+            }
+            else {
+                $null = $UsersToImport.Add($entraIdUser)
             }
         }
-
-        Write-PSFMessage -Level Verbose -Message "Getting sid from  $($entraIdUser.Mail) and identity provider : $identityProvider."
-        $sid = Get-UserSIDFromAad $entraIdUser.Mail $identityProvider
-        Write-PSFMessage -Level Verbose -Message "Generated SID : $sid"
-        $id = ""
-        if ($IdValue -eq 'Login') {
-            $id = $IdPrefix + $(Get-LoginFromEmail $entraIdUser.Mail)
-        }
-        elseif($IdValue -eq 'UserPrincipalName') {
-            $id = $IdPrefix + $entraIdUser.UserPrincipalName
-        }
-        else {
-            $id = $IdPrefix + $entraIdUser.GivenName
-        }
-        
-        if ($id.Length -gt 20) {
-            $oldId = $id
-            $id = $id -replace '^(.{0,20}).*','$1'
-            Write-PSFMessage -Level Host -Message "The id <c='em'>'$oldId'</c> does not fit the <c='em'>20 character limit</c> on UserInfo table's ID field and will be truncated to <c='em'>'$id'</c>"
-        }
-        
-        Write-PSFMessage -Level Verbose -Message "Id for user $($entraIdUser.Mail) : $id"
-
-        $name = ""
-        if ($NameValue -eq 'DisplayName') {
-            $name = $entraIdUser.DisplayName + $NameSuffix
-        }
-
-        else {
-            $name = $entraIdUser.GivenName + $NameSuffix
-        }
-        Write-PSFMessage -Level Verbose -Message "Name for user $($entraIdUser.Mail) : $name"
-        Write-PSFMessage -Level Verbose -Message "Importing $($entraIdUser.Mail) - SID $sid - Provider $identityProvider"
-
-        Import-AadUserIntoD365FO $SqlCommand $entraIdUser.Mail $name $id $sid $StartupCompany $identityProvider $networkDomain $entraIdUser.ObjectId
-
-        if (Test-PSFFunctionInterrupt) { return }
     }
+
+    Import-UsersToDatabase -Users $UsersToImport
+}
+
+function Import-GroupMembers {
+    [CmdletBinding(DefaultParameterSetName = "GroupByName")]
+    param (
+        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "GroupByName")]
+        [string] $GroupName,
+
+        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "GroupById")]
+        [string] $GroupId,
+
+        [Parameter(Mandatory = $false, Position = 1, ParameterSetName = "GroupByName")]
+        [switch] $ForceExactGroupName
+    )
+
+    switch ($PSCmdlet.ParameterSetName) {
+        "GroupByName" {
+            if ($ForceExactGroupName -eq $true) {
+                Write-PSFMessage -Level Verbose -Message "Search Entra ID gorup by its exact name : $GroupName"
+                $group = Get-MgGroup -Filter "DisplayName eq '$GroupName'"
+            }
+            else {
+                Write-PSFMessage -Level Verbose -Message "Search Entra ID group by searching within its name : $GroupName"
+                $group = Get-MgGroup -ConsistencyLevel eventual -Search "DisplayName:$GroupName" # equivalent to `Get-AzureADGroup -SearchString $GroupName`, see https://learn.microsoft.com/en-us/powershell/module/azuread/get-azureadgroup?view=azureadps-2.0#example-2-get-groups-that-contain-a-search-string
+            }
+        }
+        "GroupById" {
+            Write-PSFMessage -Level Verbose -Message "Search Entra ID group by its id : $GroupId"
+            $group = Get-MgGroup -GroupId $GroupId
+        }
+    }
+
+    if ($null -eq $group) {
+        Write-PSFMessage -Level Host -Message "Unable to find the specified group in the AAD. Please ensure the group exists and that you have enough permissions to access it."
+        Stop-PSFFunction -StepsUpward 1 -Message "Stopping because of errors"
+        return
+    }
+
+    if ($group.Length -gt 1) {
+        Write-PSFMessage -Level Host -Message "More than one group found"
+        foreach ($foundGroup in $group) {
+            Write-PSFMessage -Level Host -Message "Group found $($foundGroup.DisplayName)"
+        }
+        Stop-PSFFunction -StepsUpward 1 -Message "Stopping because of errors"
+        return
+    }
+
+    Write-PSFMessage -Level Host -Message "Processing Entra ID user group `"$($group.DisplayName)`""
+    $groupMembers = Get-MgGroupMember -GroupId $group.Id
+    $UsersToImport = New-Object -TypeName "System.Collections.ArrayList"
+    foreach ($groupMember in $groupMembers) {
+        $type = $null
+        $null = $groupMember.AdditionalProperties.TryGetValue("@odata.type", [ref]$type)
+        if ($type -eq "#microsoft.graph.user") {
+          $groupMemberUser = Get-MgUser -UserId $groupMember.Id
+          $null = $UsersToImport.Add($groupMemberUser)
+        }
+    }
+    Write-PSFMessage -Level Host -Message "Importing $($UsersToImport.Count) users from group `"$($group.DisplayName)`""
+    Import-UsersToDatabase -Users $UsersToImport
+}
+
+function Import-UsersToDatabase {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [object[]] $Users
+    )
+
+    try {
+        $sqlCommand.Connection.Open()
+
+        foreach ($user in $Users) {
+
+            $identityProvider = $canonicalProvider
+
+            Write-PSFMessage -Level Verbose -Message "Getting tenant from $($user.Mail)."
+            $tenant = Get-TenantFromEmail $user.Mail
+
+            Write-PSFMessage -Level Verbose -Message "Getting domain from $($user.Mail)."
+            $networkDomain = Get-NetworkDomain $user.Mail
+
+            Write-PSFMessage -Level Verbose -Message "InstanceProvider : $InstanceProvider"
+            Write-PSFMessage -Level Verbose -Message "Tenant : $Tenant"
+
+            if ($user.Mail.ToLower().Contains("outlook.com") -eq $true) {
+                $identityProvider = "live.com"
+            }
+            else {
+                if ($instanceProvider.ToLower().Contains($tenant.ToLower()) -ne $True) {
+                    Write-PSFMessage -Level Verbose -Message "Getting identity provider from  $($user.Mail)."
+                    $identityProvider = Get-IdentityProvider $user.Mail
+                }
+            }
+
+            Write-PSFMessage -Level Verbose -Message "Getting sid from  $($user.Mail) and identity provider : $identityProvider."
+            $sid = Get-UserSIDFromAad $user.Mail $identityProvider
+            Write-PSFMessage -Level Verbose -Message "Generated SID : $sid"
+            $id = ""
+            if ($IdValue -eq 'Login') {
+                $id = $IdPrefix + $(Get-LoginFromEmail $user.Mail)
+            }
+            elseif($IdValue -eq 'UserPrincipalName') {
+                $id = $IdPrefix + $user.UserPrincipalName
+            }
+            else {
+                $id = $IdPrefix + $user.GivenName
+            }
+            if ($id.Length -gt 20) {
+                $oldId = $id
+                $id = $id -replace '^(.{0,20}).*','$1'
+                Write-PSFMessage -Level Host -Message "The id <c='em'>'$oldId'</c> does not fit the <c='em'>20 character limit</c> on UserInfo table's ID field and will be truncated to <c='em'>'$id'</c>"
+            }
+            Write-PSFMessage -Level Verbose -Message "Id for user $($user.Mail) : $id"
+
+            $name = ""
+            if ($NameValue -eq 'DisplayName') {
+                $name = $user.DisplayName + $NameSuffix
+            }
+            else {
+                $name = $user.GivenName + $NameSuffix
+            }
+            Write-PSFMessage -Level Verbose -Message "Name for user $($user.Mail) : $name"
+
+            Write-PSFMessage -Level Verbose -Message "Importing $($user.Mail) - SID $sid - Provider $identityProvider"
+
+            Import-AadUserIntoD365FO $SqlCommand $user.Mail $name $id $sid $StartupCompany $identityProvider $networkDomain $user.Id
+
+            if (Test-PSFFunctionInterrupt) { return }
+        }
+    }
+    catch {
+        Write-PSFMessage -Level Host -Message "Something went wrong while working against the database" -Exception $PSItem.Exception
+        Stop-PSFFunction -StepsUpward 1 -Message "Stopping because of errors"
+        return
+    }
+    finally {
+        if ($sqlCommand.Connection.State -ne [System.Data.ConnectionState]::Closed) {
+            $sqlCommand.Connection.Close()
+        }
+        $sqlCommand.Dispose()
+    }
+
 }
