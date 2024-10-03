2d200f40f6b686dd9753a53137694d0ee84e9e6a
Florian Hopfner <florian.hopfner@inway.de>
2024-10-03 13:43:13 +0200

    add -TenantId parameter documentation and example

--- d365fo.tools/functions/import-d365aaduser.ps1
+++ d365fo.tools/functions/import-d365aaduser.ps1
@@ -68,6 +68,11 @@
         Available options 'Mail' / 'UserPrincipalName'
         
         Default is 'Mail'
+
+    .PARAMETER TenantId
+        The TenantId to use when connecting to Azure Active Directory
+
+        Uses the tenant id of the current environment if not specified.
         
     .EXAMPLE
         PS C:\> Import-D365AadUser -Users "Claire@contoso.com","Allen@contoso.com"
@@ -104,6 +109,12 @@
         Imports Claire and Allen as users.
         Will NOT make you connect to the Azure Active Directory(AAD).
         The needed details will be based on the e-mail address only, and the rest will be blanked.
+
+    .EXAMPLE
+        PS C:\> Import-D365AadUser -Users "Claire@contoso.com","Allen@contoso.com" -TenantId "99999999-aaaa-bbbb-cccc-9999999999"
+        
+        Imports Claire and Allen as users. Uses tenant id "99999999-aaaa-bbbb-cccc-9999999999" 
+        when connecting to Azure Active Directory(AAD).
         
     .NOTES
         Tags: User, Users, Security, Configuration, Permission, AAD, Azure Active Directory, Group, Groups
